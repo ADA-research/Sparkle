e25ec62691b3e35948b257ae6bfc834e0b18c54e
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2019-09-25 15:33:17 +0200

    Remove information form figure that is duplicated in the caption.

--- Components/Sparkle-latex-generator-for-configuration/auto_gen_plot.py
+++ Components/Sparkle-latex-generator-for-configuration/auto_gen_plot.py
@@ -8,27 +8,26 @@ import fcntl
 if __name__ == r'__main__':
 	
 	if len(sys.argv)!=6:
-		print('Usage: %s <data_portfolio_selector_sparkle_vs_vbs_filename> <penalty_time> <vbs_name> <portfolio_selector_sparkle_name> <figure_portfolio_selector_sparkle_vs_vbs_filename>' % (sys.argv[0]))
+		print('Usage: %s <data_solver_configured_vs_default_filename> <penalty_time> <default_name> <configured_name> <figure_solver_configured_vs_default_filename>' % (sys.argv[0]))
 		exit(-1)
 	
-	data_portfolio_selector_sparkle_vs_vbs_filename = sys.argv[1]
+	data_solver_configured_vs_default_filename = sys.argv[1]
 	penalty_time = sys.argv[2]
 	upper_bound = float(penalty_time)*1.5
 	lower_bound = 0.01
-	vbs_name = sys.argv[3]
-	portfolio_selector_sparkle_name = sys.argv[4]
-	figure_portfolio_selector_sparkle_vs_vbs_filename = sys.argv[5]
+	default_name = sys.argv[3]
+	configured_name = sys.argv[4]
+	figure_solver_configured_vs_default_filename = sys.argv[5]
 	
-	output_eps_file = figure_portfolio_selector_sparkle_vs_vbs_filename + r'.eps'
-	output_pdf_file = figure_portfolio_selector_sparkle_vs_vbs_filename + r'.pdf'
+	output_eps_file = figure_solver_configured_vs_default_filename + r'.eps'
+	output_pdf_file = figure_solver_configured_vs_default_filename + r'.pdf'
 	
-	vbs_name = vbs_name.replace(r'/', r'_')
-	output_gnuplot_script = portfolio_selector_sparkle_name + r'_vs_' + vbs_name + r'.plt'
+	default_name = default_name.replace(r'/', r'_')
+	output_gnuplot_script = configured_name + r'_vs_' + default_name + r'.plt'
 	
 	fout = open(output_gnuplot_script, 'w+')
-	fout.write('set xlabel \'%s, PAR10\'' % (vbs_name) + '\n')
-	fout.write('set ylabel \'%s, PAR10\'' % (portfolio_selector_sparkle_name)  + '\n')
-	fout.write('set title \'%s vs %s\'' % (portfolio_selector_sparkle_name, vbs_name)  + '\n')
+	fout.write('set xlabel \'PAR10 (default)\'\n')
+	fout.write('set ylabel \'PAR10 (configured)\'\n')
 	fout.write('unset key'  + '\n')
 	fout.write('set xrange [%s:%s]' % (lower_bound, upper_bound) + '\n')
 	fout.write('set yrange [%s:%s]' % (lower_bound, upper_bound)  + '\n')
@@ -43,7 +42,7 @@ if __name__ == r'__main__':
 	fout.write('set arrow from %s,%s to %s,%s nohead lc rgb \'black\' lt 2' % (lower_bound, penalty_time, upper_bound, penalty_time)  + '\n')
 	fout.write('set terminal postscript eps color dashed linewidth \"Helvetica\" 20'  + '\n')
 	fout.write('set output \'%s\'' % (output_eps_file)  + '\n')
-	fout.write('plot \'%s\' with points pt 2 ps 2' % (data_portfolio_selector_sparkle_vs_vbs_filename)  + '\n')
+	fout.write('plot \'%s\' with points pt 2 ps 2' % (data_solver_configured_vs_default_filename)  + '\n')
 	fout.close()
 	
 	cmd = "gnuplot \'%s\'" % (output_gnuplot_script)
