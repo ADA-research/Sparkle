174826d2981fcf0c9c69cbc034d4a1ffcd01c6dc
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2020-02-17 14:24:13 +0100

    Remove old installation instructions from example scripts. Add old installation instructions to the user guide for future revision.

--- Examples/configure_yahsp_two_sets.sh
+++ Examples/configure_yahsp_two_sets.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Before starting Sparkle, please install the following packages with the specific versions:
-# pip3 install ConfigSpace==0.3.9 --user
-# pip3 install smac==0.6.0 --user
-# pip3 install install git+https://github.com/mlindauer/ASlibScenario --user
-# pip3 install sphinx-bootstrap-theme==0.6.0 --user
-
-# Also, please install the following software:
-# pdflatex, latex, bibtex, swig, gnuplot, gnuplot-x11
-
-
-
-
 # Navigate to the Sparkle directory
 
 # Initialise a new Sparkle platform
