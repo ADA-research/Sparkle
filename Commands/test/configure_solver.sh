0bf1da466c850fe68c1338ffc58588fae90b97b5
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2020-06-03 19:28:00 +0200

    Add basic test for configure_solver

---
+++ Commands/test/configure_solver.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Execute this script from the Sparkle directory
+
+#SBATCH --job-name=test/configure_solver.sh
+#SBATCH --output=TMP/configure_solver.sh.txt
+#SBATCH --error=TMP/configure_solver.sh.err
+#SBATCH --partition=graceADA
+#SBATCH --mem-per-cpu=3gb
+#SBATCH --exclude=
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+
+# Activate environment
+source activate sparkle_test &> /dev/null
+
+# Prepare for test
+instances_path="Examples/Resources/Instances/PTN/"
+solver_path="Examples/Resources/Solvers/PbO-CCSAT-Generic/"
+
+Commands/initialise.py > /dev/null
+Commands/add_instances.py --run-solver-later --run-extractor-later $instances_path > /dev/null
+Commands/add_solver.py --run-solver-later --deterministic 0 $solver_path > /dev/null
+
+# Configure solver
+output=$(Commands/configure_solver.py --solver $solver_path --instance-set-train $instances_path | tail -1)
+
+if [[ $output =~ [0-9] ]];
+then
+	echo "[success] configure_solver test succeeded"
+else              
+	echo "[failure] configure_solver test failed with output:"
+	echo $output
+fi
+
