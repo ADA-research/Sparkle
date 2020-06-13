3e7004e60bd4ad918eef050fa31f56676cbf4abc
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2020-06-13 10:53:25 +0200

    Move load_record command to argparse and add test case

--- Commands/load_record.py
+++ Commands/load_record.py
@@ -13,22 +13,22 @@ Contact: 	Chuan Luo, chuanluosaber@gmail.com
 import os
 import sys
 import fcntl
+import argparse
 from sparkle_help import sparkle_record_help
 from sparkle_help import sparkle_csv_merge_help
 
 if __name__ == r'__main__':
+	# Define command line arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument('record_file_path', metavar='record-file-path', type=str, help='path to the record file')
 
-	if len(sys.argv) != 2:
-		print(r'c Command error!')
-		print(r'c Usage: ' + sys.argv[0] + r' <record_file_name>')
-		sys.exit()
-
-	record_file_name = sys.argv[1]
+	# Process command line arguments
+	args = parser.parse_args()
+	record_file_name = args.record_file_path
 	if not os.path.exists(record_file_name):
 		print(r'c Record file ' + record_file_name + r' does not exist!')
-		print(r'c Nothing Changed!')
 		sys.exit()
-	
+
 	print(r'c Cleaning existing Sparkle platform ...')
 	sparkle_record_help.cleanup_current_sparkle_platform()
 	print(r'c Existing Sparkle platform cleaned!')
---
+++ Commands/test/load_record.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Execute this script from the Sparkle directory
+
+#SBATCH --job-name=test/load_record.sh
+#SBATCH --output=TMP/load_record.sh.txt
+#SBATCH --error=TMP/load_record.sh.err
+#SBATCH --partition=graceADA
+#SBATCH --mem-per-cpu=3gb
+#SBATCH --exclude=
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+
+# Activate environment
+source activate sparkle_test &> /dev/null
+
+# Initialise
+record_file="Commands/test/test_files/test_record.zip"
+output_true="c Record file $record_file loaded successfully!"
+output=$(Commands/load_record.py $record_file | tail -1)
+
+if [[ $output == $output_true ]];
+then
+	echo "[success] load_record test succeeded"
+else
+	echo "[failure] load_record test failed with output:"
+	echo $output
+fi
+
