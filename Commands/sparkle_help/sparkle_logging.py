bafe0ff0838d229075577e36a8ad389b2093dee0
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2020-07-30 16:22:45 +0200

    Add header to global log file

--- Commands/sparkle_help/sparkle_logging.py
+++ Commands/sparkle_help/sparkle_logging.py
@@ -23,12 +23,18 @@ def log_command(argv, output_file=None):
 	if output_file is None:
 		output_file = 'N/A'
 	args = ' '.join(argv[0:])
-	log_str = timestamp + ' ' + args + ' ' + output_file
+	log_str = timestamp + '   ' + args + '   ' + output_file + '\n'
 
 	# Make sure directory exists
 	log_path = sgh.sparkle_global_log
 	log_dir = Path(log_path).parents[0]
 	log_dir.mkdir(parents=True, exist_ok=True)
+
+	# If the log file does not exist yet, write the header
+	if not Path(log_path).is_file():
+		log_header = '     Timestamp		  					Command		  					Output details\n'
+		with open(log_path, "a") as log_file:
+			log_file.write(log_header)
 
 	# Write to log file
 	with open(log_path, "a") as log_file:
