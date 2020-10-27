829c7e8e5e12015fab5b2e43ff72f71e4cba00d4
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2020-10-27 16:12:09 +0100

    Add argparse commandline handling to about command

--- Commands/about.py
+++ Commands/about.py
@@ -2,12 +2,19 @@
 # -*- coding: UTF-8 -*-
 
 import sys
+import argparse
 from sparkle_help import sparkle_logging as sl
 
 
 if __name__ == r'__main__':
 	# Log command call
 	sl.log_command(sys.argv)
+
+	# Define command line arguments
+	parser = argparse.ArgumentParser()
+
+	# Process command line arguments
+	args = parser.parse_args()
 
 	print('c Sparkle (Platform for evaluating empirical algorithms/solvers)')
 	print('c Version: TBD')
