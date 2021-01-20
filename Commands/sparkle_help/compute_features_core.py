c0aa47daf80ff2125db807518b7ebcde87fa63ba
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2021-01-20 11:42:48 +0100

    Move feature extraction cutoff time to new style settings

--- Commands/sparkle_help/compute_features_core.py
+++ Commands/sparkle_help/compute_features_core.py
@@ -23,7 +23,6 @@ try:
 	from sparkle_help import sparkle_basic_help
 	from sparkle_help import sparkle_file_help as sfh
 	from sparkle_help import sparkle_feature_data_csv_help as sfdcsv
-	from sparkle_help import sparkle_experiments_related_help as ser
 	from sparkle_help import sparkle_compute_features_help as scf
 	from sparkle_help import sparkle_settings
 except ImportError:
@@ -31,7 +30,6 @@ except ImportError:
 	import sparkle_basic_help
 	import sparkle_file_help as sfh
 	import sparkle_feature_data_csv_help as sfdcsv
-	import sparkle_experiments_related_help as ser
 	import sparkle_compute_features_help as scf
 	import sparkle_settings
 
@@ -58,9 +56,10 @@ if __name__ == r'__main__':
 	feature_data_csv = sfdcsv.Sparkle_Feature_Data_CSV(feature_data_csv_path)
 	runsolver_path = sgh.runsolver_path
 
-	# TODO: Move cutoff_time_total_extractor and cutoff_time_total_extractor_run_on_one_instance to new style settings
-	if len(sgh.extractor_list)==0: cutoff_time_each_extractor_run = ser.cutoff_time_total_extractor_run_on_one_instance + 1
-	else: cutoff_time_each_extractor_run = ser.cutoff_time_total_extractor_run_on_one_instance/len(sgh.extractor_list) + 1
+	if len(sgh.extractor_list) == 0:
+		cutoff_time_each_extractor_run = sgh.settings.get_general_extractor_cutoff_time()
+	else:
+		cutoff_time_each_extractor_run = sgh.settings.get_general_extractor_cutoff_time() / len(sgh.extractor_list)
 	cutoff_time_each_run_option = r'-C ' + str(cutoff_time_each_extractor_run)
 	
 	key_str = sfh.get_last_level_directory_name(extractor_path) + r'_' + sfh.get_last_level_directory_name(instance_path) + r'_' + sparkle_basic_help.get_time_pid_random_string()
--- Commands/sparkle_help/sparkle_compute_features_parallel_help.py
+++ Commands/sparkle_help/sparkle_compute_features_parallel_help.py
@@ -16,7 +16,6 @@ try:
 	from sparkle_help import sparkle_global_help as sgh
 	from sparkle_help import sparkle_file_help as sfh
 	from sparkle_help import sparkle_feature_data_csv_help as sfdcsv
-	from sparkle_help import sparkle_experiments_related_help as ser
 	from sparkle_help import sparkle_job_help
 	from sparkle_help import sparkle_compute_features_help as scf
 	from sparkle_help import sparkle_slurm_help as ssh
@@ -24,7 +23,6 @@ except ImportError:
 	import sparkle_global_help as sgh
 	import sparkle_file_help as sfh
 	import sparkle_feature_data_csv_help as sfdcsv
-	import sparkle_experiments_related_help as ser
 	import sparkle_job_help
 	import sparkle_compute_features_help as scf
 	import sparkle_slurm_help as ssh
@@ -47,8 +45,10 @@ def computing_features_parallel(feature_data_csv_path, mode):
 	####
 	# set the options to the runsolver software
 	runsolver_path = sgh.runsolver_path
-	if len(sgh.extractor_list)==0: cutoff_time_each_extractor_run = ser.cutoff_time_total_extractor_run_on_one_instance + 1
-	else: cutoff_time_each_extractor_run = ser.cutoff_time_total_extractor_run_on_one_instance/len(sgh.extractor_list) + 1
+	if len(sgh.extractor_list) == 0:
+		cutoff_time_each_extractor_run = sgh.settings.get_general_extractor_cutoff_time()
+	else:
+		cutoff_time_each_extractor_run = sgh.settings.get_general_extractor_cutoff_time() / len(sgh.extractor_list)
 	cutoff_time_each_run_option = r'-C ' + str(cutoff_time_each_extractor_run)
 	print('c Cutoff time for each run on computing features is set to ' + str(cutoff_time_each_extractor_run) + ' seconds') # print the information about the cutoff time
 	####
