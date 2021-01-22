44519b0d1c50a4942cade9c246d6684095642293
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2021-01-22 16:12:04 +0100

    Add command line option to supply settings file to compute_marginal_contribution command

--- Commands/compute_marginal_contribution.py
+++ Commands/compute_marginal_contribution.py
@@ -14,6 +14,7 @@ import sys
 import argparse
 from typing import List
 from typing import Tuple
+from pathlib import Path
 
 from sparkle_help import sparkle_global_help as sgh
 from sparkle_help import sparkle_compute_marginal_contribution_help as scmc
@@ -67,12 +68,15 @@ if __name__ == r'__main__':
 	group.add_argument('--actual', action='store_true', help='compute the marginal contribution for the actual selector')
 	parser.add_argument('--recompute', action='store_true', help='force marginal contribution to be recomputed even when it already exists in file for for the current selector')
 	parser.add_argument('--performance-measure', choices=PerformanceMeasure.__members__, default=sgh.settings.DEFAULT_general_performance_measure, action=ac.SetByUser, help='the performance measure, e.g. runtime')
+	parser.add_argument('--settings-file', type=Path, default=sgh.settings.DEFAULT_settings_path, action=ac.SetByUser, help='specify the settings file to use in case you want to use one other than the default')
 
 	# Process command line arguments
 	args = parser.parse_args()
 	flag_compute_perfect = args.perfect
 	flag_compute_actual = args.actual
 	flag_recompute = args.recompute
+
+	if ac.set_by_user(args, 'settings_file'): sgh.settings.read_settings_ini(args.settings_file, SettingState.CMD_LINE) # Do first, so other command line options can override settings from the file
 	if ac.set_by_user(args, 'performance_measure'): sgh.settings.set_general_performance_measure(PerformanceMeasure.from_str(args.performance_measure), SettingState.CMD_LINE)
 
 	compute_marginal_contribution(flag_compute_perfect, flag_compute_actual, flag_recompute)
