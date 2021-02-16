220864f4aa411f4a82ca57ac857b19ddf35852f1
Richard Middelkoop <middelkoop@fs.grace.liacs.nl>
2021-02-16 10:40:24 +0100

    added parsers

--- Commands/construct_sparkle_parallel_portfolio.py
+++ Commands/construct_sparkle_parallel_portfolio.py
@@ -4,15 +4,20 @@
 '''
 Software: 	Sparkle (Platform for evaluating empirical algorithms/solvers)
 '''
-
 import sys
+import argparse
 
 from sparkle_help import sparkle_logging as sl
 
 if __name__ == r'__main__':
     # Log command call
-	sl.log_command(sys.argv)
+    sl.log_command(sys.argv)
+
+    # Define command line arguments
+    parser = argparse.ArgumentParser()
+
+    # Process command line arguments
+    args = parser.parse_args()
 
     print('c Start constructing Sparkle parallel portfolio ...')
-
     print('c TODO ...')
--- Commands/run_sparkle_parallel_portfolio.py
+++ Commands/run_sparkle_parallel_portfolio.py
@@ -6,13 +6,19 @@ Software: 	Sparkle (Platform for evaluating empirical algorithms/solvers)
 '''
 
 import sys
+import argparse
 
 from sparkle_help import sparkle_logging as sl
 
 if __name__ == r'__main__':
     # Log command call
-	sl.log_command(sys.argv)
+    sl.log_command(sys.argv)
+
+    # Define command line arguments
+    parser = argparse.ArgumentParser()
+
+    # Process command line arguments
+    args = parser.parse_args()
 
     print('c Sparkle parallel portfolio is running ...')
-
     print('c TODO ...')
