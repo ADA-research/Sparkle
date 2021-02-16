d5bf80ca14dd0c732d89a15783dd92af01ce2291
Richard Middelkoop <middelkoop@fs.grace.liacs.nl>
2021-02-16 10:19:30 +0100

    Initialised two new commands

---
+++ Commands/construct_sparkle_parallel_portfolio.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+'''
+Software: 	Sparkle (Platform for evaluating empirical algorithms/solvers)
+'''
+
+import sys
+
+from sparkle_help import sparkle_logging as sl
+
+if __name__ == r'__main__':
+    # Log command call
+	sl.log_command(sys.argv)
+
+    print('c Start constructing Sparkle parallel portfolio ...')
+
+    print('c TODO ...')
---
+++ Commands/run_sparkle_parallel_portfolio.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+'''
+Software: 	Sparkle (Platform for evaluating empirical algorithms/solvers)
+'''
+
+import sys
+
+from sparkle_help import sparkle_logging as sl
+
+if __name__ == r'__main__':
+    # Log command call
+	sl.log_command(sys.argv)
+
+    print('c Sparkle parallel portfolio is running ...')
+
+    print('c TODO ...')
