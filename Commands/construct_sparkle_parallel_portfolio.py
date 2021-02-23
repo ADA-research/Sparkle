7f96cfa5de8666e5b5032b5c4dac16313f1e11bf
Richard Middelkoop <middelkoop@fs.grace.liacs.nl>
2021-02-23 18:12:56 +0100

    Added the setting of the scenario to consturct PaP

--- Commands/construct_sparkle_parallel_portfolio.py
+++ Commands/construct_sparkle_parallel_portfolio.py
@@ -6,11 +6,13 @@ Software: 	Sparkle (Platform for evaluating empirical algorithms/solvers)
 '''
 import sys
 import argparse
+from pathlib import Path
 
 from sparkle_help import sparkle_logging as sl
 from sparkle_help import sparkle_global_help as sgh
 from sparkle_help import sparkle_settings
 from sparkle_help.reporting_scenario import ReportingScenario
+from sparkle_help.reporting_scenario import Scenario
 
 if __name__ == r'__main__':
     # Initialise settings
@@ -33,3 +35,19 @@ if __name__ == r'__main__':
 
     print('c Start constructing Sparkle parallel portfolio ...')
     print('c TODO ...')
+
+    #TODO construct portfolio.
+    success = 1
+    
+    if success:
+        print('c Sparkle portfolio selector constructed!')
+        print('c Sparkle portfolio selector located at ' + sgh.sparkle_portfolio_selector_path)
+        
+        # Update latest scenario
+        sgh.latest_scenario.set_selection_portfolio_path(Path(sgh.sparkle_portfolio_selector_path))
+        sgh.latest_scenario.set_latest_scenario(Scenario.SELECTION) #is het eigenlijk wel selection?
+        # Set to default to overwrite possible old path
+        sgh.latest_scenario.set_selection_test_case_directory()
+
+
+    print('c After adding into scenario')
