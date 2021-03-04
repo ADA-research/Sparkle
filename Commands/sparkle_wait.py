3ce2a88eb06a677a9159d3a46b709692bb4373a6
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2021-03-04 11:36:40 +0100

    Add option to wait for dependencies of a specific command

--- Commands/sparkle_wait.py
+++ Commands/sparkle_wait.py
@@ -24,14 +24,19 @@ if __name__ == r'__main__':
 
 	# Define command line arguments
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--job-id', required=False, type=str, default=None, help='job ID to wait for')
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument('--job-id', required=False, type=str, default=None, help='job ID to wait for')
+	group.add_argument('--command', required=False, choices=CommandName.__members__, default=None, help='command you want to run, Sparkl will wait for the dependencies of this command to be completed')
 
 	# Process command line arguments
 	args = parser.parse_args()
 	job_id = args.job_id
+	command = CommandName.from_str(args.command)
 
 	if job_id is not None:
 		sjh.wait_for_job(job_id)
+	elif command is not None:
+		sjh.wait_for_dependencies(command)
 	else:
 		wait_for_all_jobs()
 
