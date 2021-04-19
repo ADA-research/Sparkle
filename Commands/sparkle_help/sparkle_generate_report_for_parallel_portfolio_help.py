ff6ecedb837a74405864ee1848991d688c7be70b
Richard Middelkoop <middelkoop@fs.grace.liacs.nl>
2021-04-19 23:37:33 +0200

    Update on the report but no instance support

--- Commands/sparkle_help/sparkle_generate_report_for_parallel_portfolio_help.py
+++ Commands/sparkle_help/sparkle_generate_report_for_parallel_portfolio_help.py
@@ -10,6 +10,7 @@ from sparkle_help import sparkle_performance_data_csv_help as spdcsv
 from sparkle_help import sparkle_compute_marginal_contribution_help as scmch
 from sparkle_help import sparkle_logging as sl
 from sparkle_help import sparkle_generate_report_help as sgrh
+from sparkle_help.sparkle_settings import PerformanceMeasure
 import compute_marginal_contribution as cmc
 
 
@@ -29,7 +30,11 @@ def get_solverList(parallel_portfolio_path: str):
 	solver_list = sfh.get_solver_list_from_parallel_portfolio(parallel_portfolio_path)
 	for solver_path in solver_list:
 		solver_name = sfh.get_file_name(solver_path)
+		if solver_name == "": solver_name = sfh.get_last_level_directory_name(solver_path)
+		x = solver_name.rfind("_")
+		if str(x) != '-1':solver_name = solver_name[:x] + '\\' + solver_name[x:]
 		str_value += r'\item \textbf{' + solver_name + r'}' + '\n'
+		print(str_value)
 	return str_value
 
 def get_numInstanceClasses(instances: list):
@@ -89,42 +94,111 @@ def get_solversWithSolution():
 	instance = sfh.get_file_name(result_lines[0])
 	solver = sfh.get_file_name(result_lines[1])
 	duration = result_lines[2]
-	str_value += r'\item \textbf{' + instance + r'}, was solved by: ' + r'\textbf{' + solver + r'} in ' + duration + ' seconds \n'
-
+	#TODO #URGENT rewrite this function for multiple instances
+	if sgh.settings.get_general_performance_measure() == PerformanceMeasure.QUALITY_ABSOLUTE:
+		str_value += r'\item \textbf{' + instance + r'}, was scored by: ' + r'\textbf{' + solver + r'} with a score of ' + duration
+	else:
+		str_value += r'\item Solver \textbf{' + solver + r'}, was the best solver on ' + r'\textbf{' + '1' + r'} instance(s)'
 
 	return str_value
 
-def get_figure_parallel_portfolio_vs_average_sequential(parallel_portfolio_path: str):
+def get_dict_sbs_penalty_time_on_each_instance(instances: list):
+	mydict = {}
+
+	cutoff_time = sgh.settings.get_general_target_cutoff_time()
+	results = get_results()
+
+	for instance in instances:
+		#TODO Create this for multiple instances.!! SBS so get_results doesnt work anymore since it finds only the PaP results.
+		this_run_time = results[2]
+		if float(this_run_time) <= cutoff_time:
+			mydict[instance] = this_run_time
+		else:
+			mydict[instance] = sgh.settings.get_penalised_time()	
+	
+	return mydict
+
+# def get_dict_vbs_penalty_time_on_each_instance():
+# 	performance_data_csv = spdcsv.Sparkle_Performance_Data_CSV(sgh.performance_data_csv_path)
+# 	mydict = performance_data_csv.get_dict_vbs_penalty_time_on_each_instance()
+# 	return mydict
+
+def get_dict_actual_portfolio_selector_penalty_time_on_each_instance(parallel_portfolio_path: str, instances: str):
+	mydict = {}
+
+	cutoff_time = sgh.settings.get_general_target_cutoff_time()
+	results = get_results()
+
+	for instance in instances:
+		#TODO Create this for multiple instances.!! SBS so get_results doesnt work anymore since it finds only the PaP results.
+		this_run_time = results[2]
+		if float(this_run_time) <= cutoff_time:
+			mydict[instance] = this_run_time
+		else:
+			mydict[instance] = sgh.settings.get_penalised_time()	
+	
+
+	return mydict
+
+def get_figure_parallel_portfolio_sparkle_vs_sbs(parallel_portfolio_path: str, instances: list):
 	str_value = r''
-	
+	dict_sbs_penalty_time_on_each_instance = get_dict_sbs_penalty_time_on_each_instance(instances)
+
+	print(dict_sbs_penalty_time_on_each_instance)
+	dict_actual_portfolio_selector_penalty_time_on_each_instance = get_dict_actual_portfolio_selector_penalty_time_on_each_instance(parallel_portfolio_path, instances)
+
 	latex_directory_path = r'Components/Sparkle-latex-generator-for-parallel-portfolio/'
-	figure_best_solvers_vs_average_solver_order_filename = r'figure_best_solvers_vs_average_solver'
-	data_best_solvers_vs_average_solver_order_filename = r'data_best_solvers_vs_average_solver.dat'
-	data_best_solvers_vs_average_solver_order_filepath = latex_directory_path + data_best_solvers_vs_average_solver_order_filename
+	figure_portfolio_selector_sparkle_vs_sbs_filename = r'figure_parallel_portfolio_sparkle_vs_sbs'
+	data_portfolio_selector_sparkle_vs_sbs_filename = r'data_parallel_portfolio_sparkle_vs_sbs_filename.dat'
+	data_portfolio_selector_sparkle_vs_sbs_filepath = latex_directory_path + data_portfolio_selector_sparkle_vs_sbs_filename
 	
-	fout = open(data_best_solvers_vs_average_solver_order_filepath, 'w+')
-	result_lines = get_results()
-	best_solver_time = result_lines[2]
-	nr_of_solvers = get_numSolvers(parallel_portfolio_path)
-	cutoff_time = sgrh.get_performanceComputationCutoffTime()
-	max_sequential_time = (int(nr_of_solvers) - 1) * int(cutoff_time) + float(best_solver_time)
-	average_sequential_time = (int(nr_of_solvers) - 1)/2 * int(cutoff_time) + float(best_solver_time)
-	fout.write("0 best-solver" + r' ' + str(best_solver_time) + '\n')
-	fout.write("1 worst-case" + r' ' + str(max_sequential_time) + '\n')
-	fout.write("2 average-case" + r' ' + str(average_sequential_time) + '\n')
+	fout = open(data_portfolio_selector_sparkle_vs_sbs_filepath, 'w+')
+	for instance in dict_sbs_penalty_time_on_each_instance:
+		sbs_penalty_time = dict_sbs_penalty_time_on_each_instance[instance]
+		sparkle_penalty_time = dict_actual_portfolio_selector_penalty_time_on_each_instance[instance]
+		fout.write(str(sbs_penalty_time) + r' ' + str(sparkle_penalty_time) + '\n')
 	fout.close()
 
-	gnuplot_command = r'cd ' + latex_directory_path + r'; python auto_gen_plot.py ' + data_best_solvers_vs_average_solver_order_filename + r' ' + figure_best_solvers_vs_average_solver_order_filename
-	
+	#TODO change this for multiple instances.
+	result_lines = get_results()
+	sbs_solver = sfh.get_file_name(result_lines[1])
+
+	penalised_time_str = str(sgh.settings.get_penalised_time())
+
+	gnuplot_command = r'cd ' + latex_directory_path + r'; python auto_gen_plot.py ' + data_portfolio_selector_sparkle_vs_sbs_filename + r' ' + penalised_time_str + r' ' + '\'SBS (' + sbs_solver + ')\' ' + r'Parallel-Portfolio' + r' ' + figure_portfolio_selector_sparkle_vs_sbs_filename
+
 	#print(gnuplot_command)
-	print('DEBUG gnuplot_command will now be executed')
-	print(gnuplot_command)
+	
 	os.system(gnuplot_command)
-	print('DEBUG after gnuplot_command is executed')
-	str_value = '\\includegraphics[width=0.6\\textwidth]{%s}' % (figure_best_solvers_vs_average_solver_order_filename)
+	
+	str_value = '\\includegraphics[width=0.6\\textwidth]{%s}' % (figure_portfolio_selector_sparkle_vs_sbs_filename)
 	return str_value
 
 
+# def get_figure_parallel_portfolio_sparkle_vs_vbs(parallel_portfolio_path: str):
+# 	str_value = r''
+# 	dict_vbs_penalty_time_on_each_instance = get_dict_vbs_penalty_time_on_each_instance()
+# 	dict_actual_portfolio_selector_penalty_time_on_each_instance = get_dict_actual_portfolio_selector_penalty_time_on_each_instance(parallel_portfolio_path)
+	
+# 	latex_directory_path = r'Components/Sparkle-latex-generator-for-parallel-portfolio/'
+# 	figure_portfolio_selector_sparkle_vs_vbs_filename = r'figure_parallel_portfolio_sparkle_vs_vbs'
+# 	data_portfolio_selector_sparkle_vs_vbs_filename = r'data_parallel_portfolio_sparkle_vs_vbs_filename.dat'
+# 	data_portfolio_selector_sparkle_vs_vbs_filepath = latex_directory_path + data_portfolio_selector_sparkle_vs_vbs_filename
+	
+# 	fout = open(data_portfolio_selector_sparkle_vs_vbs_filepath, 'w+')
+# 	for instance in dict_vbs_penalty_time_on_each_instance:
+# 		vbs_penalty_time = dict_vbs_penalty_time_on_each_instance[instance]
+# 		sparkle_penalty_time = dict_actual_portfolio_selector_penalty_time_on_each_instance[instance]
+# 		fout.write(str(vbs_penalty_time) + r' ' + str(sparkle_penalty_time) + '\n')
+# 	fout.close()
+
+# 	penalised_time_str = str(sgh.settings.get_penalised_time())
+# 	gnuplot_command = r'cd ' + latex_directory_path + r'; python auto_gen_plot.py ' + data_portfolio_selector_sparkle_vs_vbs_filename + r' ' + penalised_time_str + r' ' + r'VBS' + r' ' + r'Sparkle_Selector' + r' ' + figure_portfolio_selector_sparkle_vs_vbs_filename
+	
+# 	os.system(gnuplot_command)
+	
+# 	str_value = '\\includegraphics[width=0.6\\textwidth]{%s}' % (figure_portfolio_selector_sparkle_vs_vbs_filename)
+# 	return str_value
 
 
 def get_dict_variable_to_value(parallel_portfolio_path: str, instances: list):
@@ -137,33 +211,43 @@ def get_dict_variable_to_value(parallel_portfolio_path: str, instances: list):
 	variable = r'sparkle'
 	str_value = sgrh.get_sparkle()
 	mydict[variable] = str_value
-
+	
 	variable = r'numSolvers'
 	str_value = get_numSolvers(parallel_portfolio_path)
 	mydict[variable] = str_value
-
+	
 	variable = r'solverList'
 	str_value = get_solverList(parallel_portfolio_path)
 	mydict[variable] = str_value
-
+	
 	variable = r'numInstanceClasses'
 	str_value = get_numInstanceClasses(instances)
 	mydict[variable] = str_value
-
+	
 	variable = r'instanceClassList'
 	str_value = get_instanceClassList(instances)
 	mydict[variable] = str_value
-
+	
 	variable = r'cutoffTime'
 	str_value = sgrh.get_performanceComputationCutoffTime()
 	mydict[variable] = str_value
-
+	
 	variable = r'solversWithSolution'
 	str_value = get_solversWithSolution()
 	mydict[variable] = str_value
-
-	variable = r'figureComputationComparison'
-	str_value = get_figure_parallel_portfolio_vs_average_sequential(parallel_portfolio_path)
+	print('DEBUG ' + variable)
+	variable = r'figure-parallel-portfolio-sparkle-vs-sbs'
+	str_value = get_figure_parallel_portfolio_sparkle_vs_sbs(parallel_portfolio_path, instances)
+	mydict[variable] = str_value
+	print('DEBUG ' + variable)
+	# variable = r'figure-parallel-portfolio-sparkle-vs-vbs'
+	# str_value = get_figure_parallel_portfolio_sparkle_vs_vbs(parallel_portfolio_path)
+	# mydict[variable] = str_value
+	# print('DEBUG ' + variable)
+	variable = r'testBool'
+	str_value = r'\destrue'
+	if sgh.settings.get_general_performance_measure() == PerformanceMeasure.QUALITY_ABSOLUTE:
+		str_value = r'\desfalse'
 	mydict[variable] = str_value
 
 	return mydict
--- Components/Sparkle-latex-generator-for-parallel-portfolio/auto_gen_plot.py
+++ Components/Sparkle-latex-generator-for-parallel-portfolio/auto_gen_plot.py
@@ -6,33 +6,38 @@ import sys
 import fcntl
 
 if __name__ == r'__main__':
-	print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-	if len(sys.argv)!=3:
-		print('Usage: %s <data_best_solvers_vs_average_solver> <figure_best_solvers_vs_average_solver>' % (sys.argv[0]))
+	
+	if len(sys.argv)!=6:
+		print('Usage: %s <data_parallel_portfolio_sparkle_vs_vbs_filename> <penalty_time> <vbs_name> <parallel_portfolio_sparkle_name> <figure_parallel_portfolio_sparkle_vs_vbs_filename>' % (sys.argv[0]))
 		exit(-1)
 	
-	data_best_solvers_vs_average_solver_order_filename = sys.argv[1]
-	figure_best_solvers_vs_average_solver_order_filename = sys.argv[2]
+	data_parallel_portfolio_sparkle_vs_vbs_filename = sys.argv[1]
+	penalty_time = sys.argv[2]
+	vbs_name = sys.argv[3]
+	parallel_portfolio_sparkle_name = sys.argv[4]
+	figure_parallel_portfolio_sparkle_vs_vbs_filename = sys.argv[5]
 	
-	output_eps_file = figure_best_solvers_vs_average_solver_order_filename + r'.eps'
-	output_pdf_file = figure_best_solvers_vs_average_solver_order_filename + r'.pdf'
+	output_eps_file = figure_parallel_portfolio_sparkle_vs_vbs_filename + r'.eps'
+	output_pdf_file = figure_parallel_portfolio_sparkle_vs_vbs_filename + r'.pdf'
 	
-	output_gnuplot_script = 'figure_best_solvers_vs_average_solver.plt'
-	# So far so good
+	vbs_name = vbs_name.replace(r'/', r'_')
+	output_gnuplot_script = parallel_portfolio_sparkle_name + r'_vs_' + vbs_name + r'.plt'
+	
 	fout = open(output_gnuplot_script, 'w+')
-	fout.write('set title \'parallel vs sequential\''  + '\n')
-	fout.write('set boxwidth 0.5' + '\n')
-	fout.write('set key outside right' + '\n')
-	fout.write('set xtics rotate by 45 right' + '\n')
-	fout.write('set style fill solid' + '\n')
-	fout.write('set yrange [0:]' + '\n')
-	fout.write('set ylabel "wallclock time (s)" ' + '\n')
+	fout.write('set xlabel \'%s, PAR10\'' % (vbs_name) + '\n')
+	fout.write('set ylabel \'%s, PAR10\'' % (parallel_portfolio_sparkle_name)  + '\n')
+	fout.write('set title \'%s vs %s\'' % (parallel_portfolio_sparkle_name, vbs_name)  + '\n')
+	fout.write('unset key'  + '\n')
+	fout.write('set xrange [0.01:%s]' % (penalty_time) + '\n')
+	fout.write('set yrange [0.01:%s]' % (penalty_time)  + '\n')
+	fout.write('set logscale x'  + '\n')
+	fout.write('set logscale y'  + '\n')
+	fout.write('set grid'  + '\n')
+	fout.write('set size square'  + '\n')
+	fout.write('set arrow from 0.01,0.01 to %s,%s nohead lc rgb \'black\'' % (penalty_time, penalty_time)  + '\n')
 	fout.write('set terminal postscript eps color solid linewidth \"Helvetica\" 20'  + '\n')
 	fout.write('set output \'%s\'' % (output_eps_file)  + '\n')
-	fout.write('set linetype 1 lc rgb "red"' + '\n')
-	fout.write('set linetype 2 lc rgb "blue"' + '\n')
-	fout.write('set linetype 3 lc rgb "blue"' + '\n')
-	fout.write('plot \'%s\'' % (data_best_solvers_vs_average_solver_order_filename)  + ' using 1:3:($1+1):xtic(2) with boxes title "parallel" linecolor variable, NaN ls 2 with boxes title "sequential" \n')
+	fout.write('plot \'%s\'' % (data_parallel_portfolio_sparkle_vs_vbs_filename)  + '\n')
 	fout.close()
 	
 	cmd = "gnuplot \'%s\'" % (output_gnuplot_script)
@@ -42,3 +47,4 @@ if __name__ == r'__main__':
 	os.system(cmd)
 	
 	os.system('rm -f \'%s\'' % (output_gnuplot_script))
+	
