fe6bc20fde0deedf0ac02d6119d1ce644ef8b6ce
Richard Middelkoop <richard-drahcir@hotmail.com>
2021-07-23 13:56:04 +0000

    [PaP] run_sparkle_parallel_portfolio.py updated variable name

--- Commands/run_sparkle_parallel_portfolio.py
+++ Commands/run_sparkle_parallel_portfolio.py
@@ -35,9 +35,9 @@ if __name__ == r'__main__':
 
     # Define command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--instances", default=sgh.instance_list, metavar='N', nargs="+", type=str, action=ac.SetByUser, help='Specify the instance_path(s) on which the portfolio will run.')
+    parser.add_argument("--instance-paths", default=sgh.instance_list, metavar='N', nargs="+", type=str, action=ac.SetByUser, help='Specify the instance_path(s) on which the portfolio will run.')
     parser.add_argument("--portfolio-name", default=sgh.latest_scenario.get_parallel_portfolio_path(),type=str, action=ac.SetByUser, help='Specify the name of the portfolio, if the portfolio is not in the standard location use its full path. The default is the latest Portfolio created.')
-    #TODO change default settings to a settingsvalue
+    #TODO change default settings to a getter
     parser.add_argument('--performance-measure', choices=PerformanceMeasure.__members__, default=sgh.settings.DEFAULT_general_performance_measure, action=ac.SetByUser, help='the performance measure, e.g. runtime')
     parser.add_argument("--cutoff-time", default=sgh.settings.get_general_target_cutoff_time(), type=int, action=ac.SetByUser, help='The duration the portfolio will run before the program is stopped')
 
@@ -49,37 +49,37 @@ if __name__ == r'__main__':
         if(os.path.isdir(args.portfolio_name)): 
             portfolio_path = args.portfolio_name
         else:
-            portfolio_path = Path('Sparkle_Parallel_portfolio/' + args.portfolio_name)
+            portfolio_path = Path('Sparkle_Parallel_Portfolio/' + args.portfolio_name)
             if(not os.path.isdir(portfolio_path)):
                 sys.exit("c Portfolio not found, aborting the process")
     else: portfolio_path = args.portfolio_name
     
-    if ac.set_by_user(args, 'instances'):
-        instances = []
-        for instance in args.instances:
+    if ac.set_by_user(args, 'instance_paths'):
+        instance_paths = []
+        for instance in args.instance_paths:
             if(os.path.isdir(instance)):
-                print('c Added ' + str(len(os.listdir(instance))) + ' instances from directory ' + str(instance))
+                print('c Added ' + str(len(os.listdir(instance))) + ' instance(s) from directory ' + str(instance))
                 for item in os.listdir(instance):
                         item_with_dir = str(instance) + str(item)
-                        instances.append(str(item_with_dir))
+                        instance_paths.append(str(item_with_dir))
             elif(os.path.isfile(instance)):
                 print('c Added instance ' + str(instance))
-                instances.append(str(instance))
+                instance_paths.append(str(instance))
             else:
                 instance_with_dir = 'Instances/' + instance
                 if(os.path.isdir(instance_with_dir)):
-                    print('c Added ' + str(len(os.listdir(instance_with_dir))) + ' instances from directory ' + str(instance))
+                    print('c Added ' + str(len(os.listdir(instance_with_dir))) + ' instance(s) from directory ' + str(instance))
                     for item in os.listdir(instance_with_dir):
                         item_with_dir = 'Instances/' + str(instance) + '/' + str(item)
-                        instances.append(str(item_with_dir))    
+                        instance_paths.append(str(item_with_dir))    
     else:
-        if(args.instances is None):
+        if(args.instance_paths is None):
             sys.exit("c Instances not found, aborting the process")
-        instances = args.instances
+        instance_paths = args.instance_paths
         if(os.path.isfile(sgh.used_instance_list_file)):
-            temp = [i for i in instances if i not in sfh.get_used_instance_list_from_file('Instances/')]
-            instances = temp
-        if(len(instances) == 0):
+            temp = [i for i in instance_paths if i not in sfh.get_used_instance_list_from_file('Instances/')]
+            instance_paths = temp
+        if(len(instance_paths) == 0):
             sys.exit("c No unused instances found, aborting the process")
     
     if ac.set_by_user(args, 'cutoff_time'):
@@ -90,19 +90,13 @@ if __name__ == r'__main__':
     if ac.set_by_user(args, 'performance_measure'): sgh.settings.set_general_performance_measure(PerformanceMeasure.from_str(args.performance_measure), SettingState.CMD_LINE)
     
     print('c Sparkle parallel portfolio is running ...')
-    # instances = list of paths to all instances
+    # instance_paths = list of paths to all instances
     # portfolio_path = Path to the portfolio containing the solvers
     # cutoff_time = int of the cutoff_time in seconds (for now)
-    succes = srpp.run_parallel_portfolio(instances, portfolio_path, cutoff_time)
+    succes = srpp.run_parallel_portfolio(instance_paths, portfolio_path, cutoff_time)
 
     if succes:
-        sgh.latest_scenario.set_parallel_portfolio_instance(instances)
-        if(not os.path.isfile(sgh.used_instance_list_file)):
-            sfh.create_new_empty_file(sgh.used_instance_list_file)
-            #TODO add check if instance has already been used before adding it to the file.
-        for used_instance in instances:
-            sfh.add_used_instance_into_file(used_instance)
-        
+        sgh.latest_scenario.set_parallel_portfolio_instance(instance_paths)    
         print('c Running Sparkle parallel portfolio is done!')
         
         # Write used settings to file
