76da61d084f25f1f17ad3e0b74372cdc8394a4fd
J. Rook <jrook94@gmail.com>
2021-10-27 12:57:45 +0200

    Change TMP/ to Tmp/

--- Commands/test/generate_report_for_test.sh
+++ Commands/test/generate_report_for_test.sh
@@ -3,8 +3,8 @@
 # Execute this script from the Sparkle directory
 
 #SBATCH --job-name=test/generate_report_for_test.sh
-#SBATCH --output=TMP/generate_report_for_test.sh.txt
-#SBATCH --error=TMP/generate_report_for_test.sh.err
+#SBATCH --output=Tmp/generate_report_for_test.sh.txt
+#SBATCH --error=Tmp/generate_report_for_test.sh.err
 #SBATCH --partition=graceADA
 #SBATCH --mem-per-cpu=3gb
 #SBATCH --exclude=
