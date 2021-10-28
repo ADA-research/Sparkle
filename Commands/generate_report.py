5bd0e3b60c13caf58f68886588576d15934ec364
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2021-10-28 16:50:36 +0200

    Fix issue where generate report would fail due to an undefined variable

--- Commands/generate_report.py
+++ Commands/generate_report.py
@@ -175,7 +175,7 @@ if __name__ == r"__main__":
             print(r"c Report for test generated ...")
 
         delete_task_run_status()
-    elif scenario == Scenario.PARALLELPORTFOLIO:
+    elif sgh.latest_scenario.get_latest_scenario() == Scenario.PARALLELPORTFOLIO:
         # Reporting for parallel portfolio
         sgrfpph.generate_report(str(parallel_portfolio_path), pap_instance_list)
         print(r'c Parallel portfolio report generated ...')
