d3805fd00637ee631ddbef869ec09c9fe2905a41
Sam Vermeulen <samvermeulen99@gmail.com>
2022-04-19 15:30:46 +0200

    removed commented code and made subsection for feature extractors in latex constructor

--- Examples/Resources/Solvers/spearNew/sparkle_smac_wrapper.py
+++ Examples/Resources/Solvers/spearNew/sparkle_smac_wrapper.py
@@ -7,6 +7,8 @@ import random
 import sys
 import re
 import logging
+import subprocess
+import itertools
 from pathlib import Path
 from subprocess import Popen, PIPE
 from tempfile import NamedTemporaryFile
@@ -29,6 +31,26 @@ def get_last_level_directory_name(filepath):
 	else: filepath = filepath[right_index+1:]
 	return filepath
 
+# Read runtime in CPU and WC time from runsolver values file
+def get_runtime_from_runsolver(runsolver_values_path: str) -> (float, float):
+    cpu_time = float(-1)
+    wc_time = float(-1)
+
+    with open(runsolver_values_path, 'r+') as infile:
+        while True:
+            line = infile.readline().strip()
+            if not line:
+                break
+            words = line.split('=')
+            # Read wallclock time from a line of the form 'WCTIME=0.110449'
+            if len(words) == 2 and words[0] == 'WCTIME':
+                wc_time = float(words[1])
+            # Read CPU time from a line of the form 'CPUTIME=0.110449'
+            elif len(words) == 2 and words[0] == 'CPUTIME':
+                cpu_time = float(words[1])
+                break # order is fixed, CPU is the last thing we want to read, so break from the loop
+
+    return cpu_time, wc_time
 
 def float_regex(self):
 	return '[+-]?\d+(?:\.\d+)?(?:[eE][+-]\d+)?'
@@ -50,21 +72,18 @@ tmp_directory = relative_path + r'tmp/'
 if not os.path.exists(tmp_directory):
 	os.system(r'mkdir -p ' + tmp_directory)
 
-instance_name = get_last_level_directory_name(instance)
-solver_name = get_last_level_directory_name(solver_binary)
-runsolver_watch_data_path = tmp_directory + solver_name + r'_' + instance_name + r'_' + get_time_pid_random_string() + r'.log'
 
-# command = runsolver_binary + r' -w ' + runsolver_watch_data_path + r' -W ' + str(cutoff_time) + r' ' + solver_binary + r' -inst ' + instance + r' -seed ' + str(seed)
-# command = solver_binary + r' --seed ' + str(seed) + r' --model-stdout' + r' --dimacs ' + instance 
-# cmd = "%s --seed %d --model-stdout --dimacs %s" %(solver_binary, runargs["seed"], runargs["instance"]) + rest
+instance_name = Path(instance).stem
+solver_name = Path(solver_binary).stem
+runsolver_path = tmp_directory + solver_name + '_' + instance_name + '_' + get_time_pid_random_string()
+runsolver_watch_data_path = runsolver_path + '.log'
+runsolver_value_data_path = runsolver_path + '.val'
+runsolver_solver_data_path = runsolver_path + '.raw'
+
+# builds the command for the Spear solver
 command = "%s --seed %d --model-stdout --dimacs %s" %(solver_binary, seed, instance)
 
-random_id = get_time_pid_random_string()
-watcher_file = NamedTemporaryFile(
-	suffix=".log", prefix="watcher-%s-" % (random_id), dir=tmp_directory, delete=False)
-solver_file = NamedTemporaryFile(
-	suffix=".log", prefix="solver-%s-" % (random_id), dir=tmp_directory, delete=False)
-
+# adds extra settings to the command
 len_argv = len(sys.argv)
 i = 6
 while i<len_argv:
@@ -73,97 +92,69 @@ while i<len_argv:
 	command += r' ' + sys.argv[i]
 	i += 1
 
-runsolver_cmd = [runsolver_binary, "-C", cutoff_time,
-					"-w", "\"%s\"" % (watcher_file.name),
-					"-o", "\"%s\"" % (solver_file.name)]
-
-runsolver_cmd = " ".join(map(str, runsolver_cmd)) + " " + command
-
-#print(command)
-
-subprocesses = []
-start_time = time.time()
-# output_list = os.popen(call_target(command)).readlines()
-io = Popen(runsolver_cmd, shell=True,
-			preexec_fn=os.setpgrp, universal_newlines=True)
-subprocesses.append(io)
-io.wait()
-subprocesses.remove(io)
-if io.stdout:
-	io.stdout.flush()
-solver_file.seek(0)
-watcher_file.seek(0)
-end_time = time.time()
-run_time = end_time - start_time
-if run_time > cutoff_time: run_time = cutoff_time
-
-os.system(r'rm -f ' + runsolver_watch_data_path)
-
-#print output_list
+# constructs the runsolver command
+command = runsolver_binary + ' -w ' + runsolver_watch_data_path + ' -o ' + runsolver_solver_data_path + ' -v ' + runsolver_value_data_path + ' -W ' + str(cutoff_time) + r' ' + command
+subprocess.call(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
+with Path(runsolver_solver_data_path).open('r') as infile:
+    output_list = infile.read().splitlines()
+run_time, clock_time = get_runtime_from_runsolver(runsolver_value_data_path)
 
-logger = logging.getLogger("GenericWrapper")
-logger.setLevel(logging.DEBUG)
-additional = ""
+if run_time > cutoff_time:
+    run_time = cutoff_time
 
-try:
-	data = str(watcher_file.read().decode("utf8"))
-	if (re.search('runsolver_max_cpu_time_exceeded', data) or re.search('Maximum CPU time exceeded', data)):
-		status = "TIMEOUT"
+# look for model (from genericWrapper4AC/domain_specific/satwrapper.py)
+model = None
+for line in output_list:
+    line = str(line)
+    if line.startswith("v "):
+        model = map(int, line.split(" ")[1:-1])
+        break
 
-	if (re.search('runsolver_max_memory_limit_exceeded', data) or re.search('Maximum VSize exceeded', data)):
-		status = "TIMEOUT"
-		additional += " memory limit was exceeded"
-
-	cpu_pattern1 = re.compile('^runsolver_cputime: (%s)' % (
-		float_regex()), re.MULTILINE)
-	cpu_match1 = re.search(cpu_pattern1, data)
-
-	cpu_pattern2 = re.compile('^CPU time \\(s\\): (%s)' % (
-		float_regex()), re.MULTILINE)
-	cpu_match2 = re.search(cpu_pattern2, data)
-
-	if (cpu_match1):
-		time = float(cpu_match1.group(1))
-	if (cpu_match2):
-		time = float(cpu_match2.group(1))
-
-	exitcode_pattern = re.compile('Child status: ([0-9]+)')
-	exitcode_match = re.search(exitcode_pattern, data)
-
-	if (exitcode_match):
-		exit_code = int(exitcode_match.group(1))
-except:
-	# due to the common, rare runsolver bug,
-	# the watcher file can be corrupted and can fail to be read
-	exit_code = 0
-	logger.warning(
-		"Failed to read runsolver's watcher file---trust own wc-time measurment")
-
-
-
-print("reading solver results from %s" % (solver_file.name))
-data = solver_file.read()
-try:
-    data = str(data.decode("utf8"))
-except AttributeError:
-    print("AttributeError when reading solver results from file")
-    pass    
-
+# Check status
 status = 'CRASHED'
-print(data)
-if re.search('UNSATISFIABLE', data) or re.search('SATISFIABLE', data):
-    status = 'SUCCESS'
-elif re.search('s UNKNOWN', data) or re.search('INDETERMINATE', data):
-    status = 'TIMEOUT'
+for line in output_list:
+    line = line.strip()
 
+    if (line == 's SATISFIABLE') or (line == 's UNSATISFIABLE'):
+        status = 'SUCCESS'
+        break
+    elif line.startswith('c the file does not exist:'):
+        status = 'CRASHED'
+        break
+    elif line == 's UNKNOWN':
+        status = 'TIMEOUT'
+        break
+    elif model is None and run_time >= cutoff_time:
+        status = 'TIMEOUT'
+    elif run_time >= cutoff_time:
+        status = 'TIMEOUT'
+    elif clock_time >= cutoff_time:
+        status = 'TIMEOUT'
+    elif model is None:
+        status = 'TIMEOUT'
+
+if status == 'SUCCESS' or status == 'TIMEOUT':
+    # Remove runsolver and raw solver output
+    Path(runsolver_watch_data_path).unlink()
+    Path(runsolver_value_data_path).unlink()
+    # Solver output with solution (when succesful), remove because only used in configuration
+    Path(runsolver_solver_data_path).unlink()
+
+    # Remove files generated by SparrowToRiss when terminated by runsolver,
+    # but executed correctly
+    pathlist = Path(tmp_directory).glob('cp_*')
+    pathlist2 = Path(tmp_directory).glob('model_*')
+
+    for path in itertools.chain(pathlist, pathlist2):
+        Path(path).unlink()
 
 print('Result for SMAC: ' + status + ', ' + str(run_time) + ', 0, 0, ' + str(seed))
 
 if specifics == 'rawres':
-	raw_result_path = Path(runsolver_watch_data_path.replace('.log', '.rawres'))
+    raw_result_path = Path(runsolver_watch_data_path.replace('.log', '.rawres'))
 
-	with raw_result_path.open('w') as outfile:
-		for line in data:
-			outfile.write(line)
+    with raw_result_path.open('w') as outfile:
+        for line in output_list:
+            outfile.write(line)
