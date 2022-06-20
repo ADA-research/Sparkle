1d54ac9d25ba99795e003394f45c5d96019a9f0a
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2022-06-20 17:06:36 +0200

    Change paths in conf files so sources are found. Also fix warning about language setting. HTML building should now work (with warnings), LaTeX building is still broken.

--- Documentation/source/userguide/conf.py
+++ Documentation/source/userguide/conf.py
@@ -16,9 +16,9 @@ import os
 import sys
 
 # Add path to sparkle root
+sys.path.insert(0, os.path.abspath('../../../..'))
 sys.path.insert(0, os.path.abspath('../../..'))
-sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../Commands'))
+sys.path.insert(0, os.path.abspath('../../../Commands'))
 print('sys.path: ', sys.path)
 
 # -- Project information -----------------------------------------------------
@@ -72,7 +72,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en-gb'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
