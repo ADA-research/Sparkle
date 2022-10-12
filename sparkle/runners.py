01be06c29d687d258390189eca91b2c8256044ed
Koen van der Blom <k.van.der.blom@liacs.leidenuniv.nl>
2022-10-12 16:32:04 +0200

    Remove dead code

--- sparkle/runners.py
+++
@@ -1,8 +0,0 @@
-''' Class and functions to manage the external code runners '''
-from enum import Enum
-
-
-class Runners(str, Enum):
-    ''' Enumeration of the possible runners '''
-    LOCAL = 'local'
-    SLURM = 'slurm'
