4f70f16f3dd44aff6d9ce9961a116156776eb999
Malte Schwerin <schwerin.malte@land-jungs.de>
2022-11-30 11:13:07 +0100

    remove unused import

--- Commands/validate_configured_vs_default.py
+++ Commands/validate_configured_vs_default.py
@@ -7,7 +7,6 @@ from pathlib import Path
 
 from sparkle_help import sparkle_global_help as sgh
 from sparkle_help import sparkle_configure_solver_help as scsh
-from sparkle_help import sparkle_file_help as sfh
 from sparkle_help import sparkle_instances_help as sih
 from sparkle_help import sparkle_slurm_help as ssh
 from sparkle_help import sparkle_logging as sl
