b0445cba3bf591f0be08f2692316a0b97b45949d
Malte Schwerin <malte.schwerin@rwth-aachen.de>
2022-12-20 14:37:50 +0100

    fix type hints

--- Commands/sparkle_help/sparkle_file_help.py
+++ Commands/sparkle_help/sparkle_file_help.py
@@ -117,7 +117,7 @@ def get_instance_list_from_reference(instances_path: Path) -> List[str]:
     return instance_list
 
 
-def get_solver_list_from_parallel_portfolio(portfolio_path: Path) -> list[str]:
+def get_solver_list_from_parallel_portfolio(portfolio_path: Path) -> List[str]:
     """Return a list of solvers for a parallel portfolio specified by its path."""
     portfolio_solver_list = []
     solvers_path_str = "Solvers/"
@@ -247,7 +247,7 @@ def get_list_all_csv_filename(filepath):
     return csv_list
 
 
-def get_list_all_result_filename(filepath: str) -> list[str]:
+def get_list_all_result_filename(filepath: str) -> List[str]:
     """Return a list of result files in a given path."""
     result_list = []
     if not os.path.exists(filepath):
