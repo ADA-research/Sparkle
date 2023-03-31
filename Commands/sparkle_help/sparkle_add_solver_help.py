91aebbc0345c6eb27d76fb61f7315c384da0e865
Tarek Junied <tarekjunied@icloud.com>
2023-03-31 09:56:20 +0200

    Punctuation.

--- Commands/sparkle_help/sparkle_add_solver_help.py
+++ Commands/sparkle_help/sparkle_add_solver_help.py
@@ -14,10 +14,10 @@ def get_solver_directory(solver_name: str) -> str:
     """A helper function that returs the path to a solver.
 
     Args:
-        solver_name (str): the name of the solver for which the path is needed
+        solver_name (str): The name of the solver for which the path is needed.
 
     Returns:
-        str: the relative path of the solver as a string
+        str: The relative path of the solver as a string.
     """
     return "Solvers/" + solver_name
 
@@ -26,10 +26,10 @@ def check_adding_solver_contain_pcs_file(solver_directory: str) -> bool:
     """Returns whether the directory of the solver being added contains a PCS file.
 
     Args:
-        solver_directory (str): the directory to be checked if it contains a PCS file
+        solver_directory (str): The directory to be checked if it contains a PCS file.
     Returns:
         bool: A Boolean that is true if and only if
-        solver_directory contains exactly one PCS file
+        solver_directory contains exactly one PCS file.
     """
     list_files = os.listdir(solver_directory)
 
