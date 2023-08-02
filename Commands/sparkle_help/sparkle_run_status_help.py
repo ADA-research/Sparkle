d255c6ffed3ea8b43607a399fb00fd56a0e143db
Aaron Berger <aaronberger@hotmail.de>
2023-08-02 14:23:26 +0200

    merged job ids in output by command

--- Commands/sparkle_help/sparkle_run_status_help.py
+++ Commands/sparkle_help/sparkle_run_status_help.py
@@ -5,9 +5,32 @@
 from Commands.sparkle_help import sparkle_job_help as sjh
 
 
+def get_jobs_for_command(jobs: list[dict[str, str, str]], command: str) \
+        -> list[dict[str, str, str]]:
+    """Filter jobs by a command.
+
+    Args:
+      jobs: List of jobs
+      command: The command to filter for
+
+    Returns:
+      Jobs that belong to the given command.
+    """
+    return [x for x in jobs if x["command"] == command]
+
+
 def print_running_jobs():
     """Print all the running jobs."""
     sjh.cleanup_active_jobs()
     jobs = sjh.read_active_jobs()
-    for job in jobs:
-        print(f"The command {job['command']} is {job['state']} with job ID {job['job_id']}")
+    commands = list(set([x["command"] for x in jobs]))
+    for command in commands:
+        command_specific_jobs = get_jobs_for_command(jobs, command)
+        command_specific_jobs_ids = " ".join([x["job_id"] for x in command_specific_jobs])
+
+        if len(command_specific_jobs) > 1:
+            print(f"The command {command} is running "
+                  f"with job IDs {command_specific_jobs_ids}")
+        else:
+            print(f"The command {command} is running "
+                  f"with job ID {command_specific_jobs_ids}")
