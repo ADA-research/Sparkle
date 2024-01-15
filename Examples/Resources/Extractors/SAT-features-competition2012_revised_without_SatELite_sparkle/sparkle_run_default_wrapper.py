85869dec0928cd1ad9e4a43b910237ebbb57d8db
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-01-15 18:07:02 +0100

    [SPRK-109] Sparkle wrapper fix

--- Examples/Resources/Extractors/SAT-features-competition2012_revised_without_SatELite_sparkle/sparkle_run_default_wrapper.py
+++ Examples/Resources/Extractors/SAT-features-competition2012_revised_without_SatELite_sparkle/sparkle_run_default_wrapper.py
@@ -8,6 +8,7 @@ import random
 import time
 from pathlib import Path
 
+
 global sparkle_special_string
 sparkle_special_string = r'__@@SPARKLE@@__'
 
@@ -21,13 +22,10 @@ def get_time_pid_random_string():
 	return my_time_pid_random_str
 
 def get_last_level_directory_name(filepath):
-	if filepath[-1] == r'/':
-		filepath = filepath[0:-1]
+	if filepath[-1] == r'/': filepath = filepath[0:-1]
 	right_index = filepath.rfind(r'/')
-	if right_index < 0:
-		pass
-	else:
-		filepath = filepath[right_index+1:]
+	if right_index<0: pass
+	else: filepath = filepath[right_index+1:]
 	return filepath
 
 def deal_raw_result_file(relative_path, cnf_instance_file_name, raw_result_file_name, result_feature_file_name):
@@ -42,13 +40,10 @@ def deal_raw_result_file(relative_path, cnf_instance_file_name, raw_result_file_
 	while True:
 		myline = fin.readline()
 		myline = myline.strip()
-		if not myline:
-			break
+		if not myline: break
 		mylist = myline.split()
-		if len(mylist) == 0:
-			continue
-		elif mylist[0] == r'c':
-			continue
+		if len(mylist) == 0: continue
+		elif mylist[0] == r'c': continue
 		else: 
 			#fout.write(',' + myline + '\n')
 			mylist_comma = myline.split(r',')
@@ -67,28 +62,18 @@ def deal_raw_result_file(relative_path, cnf_instance_file_name, raw_result_file_
 
 executable_name = r'features'
 
-'''
-if len(sys.argv) !=4:
-	print r'c Command error!'
-	print r'c Usage: ' + sys.argv[0] + r' <relative_path> <cnf_instance_file> <result_feature_file>'
-	sys.exit()
-'''
-
 relative_path = sys.argv[1]
 cnf_instance_file_name = sys.argv[2]
 result_feature_file_name = sys.argv[3]
-#tmp_output = sys.argv[4]
 
 raw_result_file_name = relative_path + r'' + executable_name + r'_' + get_last_level_directory_name(cnf_instance_file_name) + r'_' + get_time_pid_random_string() + r'.rawres'
 
 tmp_output = r'TMP/' + raw_result_file_name
 
-#command_line = relative_path + r'/' + executable_name + r' ' + cnf_instance_file_name + ' ' + tmp_output + r' > ' + raw_result_file_name
-command_line = [relative_path + '/' + executable_name, cnf_instance_file_name, tmp_output, '>', raw_result_file_name]
-subprocess.run(command_line)
-# os.system(command_line)
-
-#print raw_result_file_name
+command_line = [relative_path + r'/' + executable_name,
+				cnf_instance_file_name,
+				tmp_output]
+subprocess.run(command_line, stdout=Path(raw_result_file_name).open("w+"))
 
 deal_raw_result_file(relative_path, cnf_instance_file_name, raw_result_file_name, result_feature_file_name)
 
