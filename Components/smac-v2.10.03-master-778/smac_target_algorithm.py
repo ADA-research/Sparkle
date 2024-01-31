9faafdec2ba2c28b1f84589db44b50fc27732b93
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-01-31 11:01:46 +0100

    [SPRK-69] Moving Runsolver into the target_algorithm script

--- Components/smac-v2.10.03-master-778/smac_target_algorithm.py
+++ Components/smac-v2.10.03-master-778/smac_target_algorithm.py
@@ -85,24 +85,43 @@ def parser_function() -> argparse.ArgumentParser:
 
 if __name__ == "__main__":
     # Incoming call from SMAC:
-    # Args 1-5 contain arguments for Run Solver, the rest are parameters for the solver
-    parser = parser_function()
-    args = parser.parse_args(sys.argv[6:])
-    args.instance = sys.argv[1]
-    args.specifics = sys.argv[2]
-    args.cutoff_time = float(sys.argv[3])
-    args.run_length = int(sys.argv[4])
-    args.seed = int(sys.argv[5])
+    # 1. Translate input from SMAC to standardized form
+    argsiter = iter(sys.argv[6:])
+    args = zip(argsiter, argsiter)
+    args = {arg.strip("-"): val for arg, val in args}
+    # Args 1-5 conditions of the run, the rest are configurations for the solver
+    args["instance"] = sys.argv[1]
+    args["specifics"] = sys.argv[2]
+    args["cutoff_time"] = float(sys.argv[3])
+    args["run_length"] = int(sys.argv[4])
+    args["seed"] = int(sys.argv[5])
+    cutoff_time = int(args["cutoff_time"])+1
 
+    # 2. Build Run Solver call
+    runsolver_binary = "./runsolver"
+    log_timestamp = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))
+    runsolver_watch_data_path = f"{str(Path.cwd())}_"\
+                                f"{log_timestamp}.log"
+
+    runsolver_call = [runsolver_binary,
+                      "-w", str(runsolver_watch_data_path),
+                      "--cpu-limit", str(cutoff_time),
+                      str(Path.cwd() / sgh.sparkle_solver_configurator_wrapper),
+                      str(args)]
+
+    # 3. Call Runsolver with the solver configurator wrapper and its arguments
     start_t = time.time()
-    solver = subprocess.run([Path.cwd() / sgh.sparkle_solver_configurator_wrapper,
-                             str(args.__dict__)],
-                            capture_output=True)
-    run_time = min(time.time() - start_t, args.cutoff_time)
+    run_solver = subprocess.run(runsolver_call,
+                                capture_output=True)
+    run_time = min(time.time() - start_t, cutoff_time)
+
+    # 4. Decode solver output and return required values to SMAC.
+    # Solver output can be found in the regular subprocess.stdout
+    if run_solver.returncode != 0:
+        # Failure from run solver or solver wrapper
+        print(run_solver.stderr)
+        sys.exit(run_solver.returncode)
     
-    if solver.returncode != 0:
-        print(solver.stderr)
-        sys.exit(solver.returncode)
-    
-    outdir = ast.literal_eval(solver.stdout.decode())
-    print(f"Result for SMAC: {outdir['status']}, {run_time}, 0, {outdir['quality']}, {args.seed}")
+    outdir = ast.literal_eval(run_solver.stdout.decode())
+    # Return values to SMAC
+    print(f"Result for SMAC: {outdir['status']}, {run_time}, 0, {outdir['quality']}, {args['seed']}")
--- Examples/Resources/Solvers/PbO-CCSAT-Generic/sparkle_solver_configurator_wrapper.py
+++ Examples/Resources/Solvers/PbO-CCSAT-Generic/sparkle_solver_configurator_wrapper.py
@@ -23,10 +23,8 @@ def get_time_pid_random_string():
 args = ast.literal_eval(sys.argv[1])
 
 # Extract and delete data that needs specific formatting
-instance = args["instance"]
+instance = Path(args["instance"])
 specifics = args["specifics"]
-cutoff_time = int(args["cutoff_time"])+1
-# run_length = args["run_length"]
 seed = args["seed"]
 
 del args["instance"]
@@ -35,34 +33,28 @@ del args["seed"]
 del args["specifics"]
 del args["run_length"]
 
-runsolver_binary = "./runsolver"
-solver_binary = "./PbO-CCSAT"
+solver_name = "PbO-CCSAT"
 
 tmp_directory = Path("tmp/")
 tmp_directory.mkdir(exist_ok=True)
 
-instance_name = Path(instance).name
-solver_name = Path(solver_binary).name
-runsolver_watch_data_path = tmp_directory / (solver_name + "_" + instance_name + "_" + get_time_pid_random_string() + ".log")
+rawres_file = tmp_directory / (solver_name + "_" + instance.name + "_" + get_time_pid_random_string() + ".rawres")
 
-runsolver_call = [runsolver_binary,
-                  "-w", str(runsolver_watch_data_path),
-                  "--cpu-limit", str(cutoff_time),
-                  solver_binary,
-                  "-inst", instance,
-                  "-seed", str(seed)]
+solver_cmd = ["./" + solver_name,
+              "-inst", instance,
+              "-seed", str(seed)]
 
+# Construct call from args dictionary
 params = []
 for key in args:
     if args[key] is not None:
         params.extend(["-" + str(key), str(args[key])])
 
-solver_call = subprocess.run(runsolver_call + params,
+solver_call = subprocess.run(solver_cmd + params,
                              capture_output=True)
 
+# Convert Solver output to dictionary for configurator target algorithm script
 output_list = solver_call.stdout.decode().splitlines()
-
-Path(runsolver_watch_data_path).unlink(missing_ok=True)
 
 status = r'CRASHED'
 for line in output_list:
@@ -75,14 +67,14 @@ for line in output_list:
         break
 
 if specifics == 'rawres':
-    raw_result_path = Path(runsolver_watch_data_path.replace('.log', '.rawres'))
+    raw_result_path = Path(rawres_file)
     with raw_result_path.open('w') as outfile:
         for line in output_list:
             outfile.write(line)
 
 outdir = {"status": status,
           "quality": 0,
-          "solver_call": runsolver_call + params,
+          "solver_call": solver_cmd + params,
           "raw_output": output_list}
 
 print(outdir)
