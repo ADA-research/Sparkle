4f78299157f43ea2cbf4889f37b3195b5bfb845b
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-02-09 16:50:25 +0100

    Pytest fix

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -6,8 +6,8 @@ import shutil
 
 from unittest import TestCase, mock
 from pathlib import Path
-
 from Commands.sparkle_help.solver import Solver
+from unittest.mock import patch
 
 
 class TestSolver(TestCase):
@@ -54,22 +54,16 @@ class TestSolver(TestCase):
         with self.assertRaises(SystemExit):
             solver.get_pcs_file()
 
+    @patch('Commands.sparkle_help.sparkle_global_help.solver_list', ["Solvers/test_solver 0 1"])
     def test_is_deterministic_false(self: TestSolver) -> None:
         """Test if is_deterministic() correctly returns False."""
-        file_string = "Solvers/test_solver 0 1"
         solver = Solver(self.solver_path)
-
-        with mock.patch("pathlib.Path.open",
-                        mock.mock_open(read_data=file_string)) as mock_file:
-            self.assertEqual(solver.is_deterministic(), "0")
-            mock_file.assert_called_with("r+")
-
+        self.assertEqual(solver.is_deterministic(), "0")
+    
+    @patch('Commands.sparkle_help.sparkle_global_help.solver_list', ["Solvers/test_solver 1 1"])
     def test_is_deterministic_true(self: TestSolver) -> None:
         """Test if is_deterministic() correctly returns True."""
-        file_string = "Solvers/test_solver 1 1"
+        
+       # with mock.patch("sgh.solver_list", return_value=["Solvers/test_solver 1 1"]):
         solver = Solver(self.solver_path)
-
-        with mock.patch("pathlib.Path.open",
-                        mock.mock_open(read_data=file_string)) as mock_file:
-            self.assertEqual(solver.is_deterministic(), "1")
-            mock_file.assert_called_with("r+")
+        self.assertEqual(solver.is_deterministic(), "1")
