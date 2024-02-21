999b15815813527df948dfe0b646e2fbdf701400
Noah Peil <noah.peil@rwth-aachen.de>
2024-02-21 14:49:53 +0100

    Let Sparkle give a warning if Multi Objectiveness  is not supported by Configurator

--- Commands/sparkle_help/configurator.py
+++ Commands/sparkle_help/configurator.py
@@ -38,6 +38,16 @@ class Configurator:
         self.sbatch_filename = ""
         (self.configurator_path / "tmp").mkdir(exist_ok=True)
 
+        self.multiobjective = True
+        if configurator_path == Path("sparkle/Components/smac-v2.10.03-master-778/smac"):
+            self.multiobjective = False
+
+        performance_measures = sgh.settings.get_general_sparkle_objectives()
+        if len(performance_measures) > 1 and not self.multiobjective:
+            print("Warning: Multiple objectives specified but current configurator "
+                  f"{self.configurator_path} only supports single objective.\n"
+                  "-> Default to first indicated objective.")
+
     def create_sbatch_script(self: Configurator,
                              scenario: ConfigurationScenario) -> None:
         """Create sbatch script.
