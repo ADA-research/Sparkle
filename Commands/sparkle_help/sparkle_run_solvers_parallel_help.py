b07c6b50fefa51cd48377ea75be1c75f60ca36ae
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-03-27 23:04:04 +0100

    Refactoring run solvers

--- Commands/sparkle_help/sparkle_run_solvers_parallel_help.py
+++ Commands/sparkle_help/sparkle_run_solvers_parallel_help.py
@@ -3,55 +3,14 @@
 """Helper functions for parallel execution of solvers."""
 
 from Commands.sparkle_help import sparkle_global_help as sgh
-from Commands.sparkle_help import sparkle_basic_help as sbh
 from Commands.sparkle_help import sparkle_performance_data_csv_help as spdcsv
 from Commands.sparkle_help import sparkle_job_help as sjh
 from Commands.sparkle_help import sparkle_run_solvers_help as srs
 from Commands.sparkle_help import sparkle_slurm_help as ssh
-from Commands.sparkle_help import sparkle_logging as sl
 from Commands.sparkle_help.sparkle_command_help import CommandName
 
-from sparkle.slurm_parsing import SlurmBatch
 import runrunner as rrr
 from runrunner.base import Runner
-
-
-def generate_running_solvers_sbatch_shell_script(total_job_num: int,
-                                                 num_job_in_parallel: int,
-                                                 total_job_list: list[tuple[str, str]],
-                                                 ) -> (str, str, str):
-    """Generate a Slurm shell script to run the solvers."""
-    sbatch_script_name = ("running_solvers_sbatch_shell_script_"
-                          f"{sbh.get_time_pid_random_string()}.sh")
-    sbatch_script_path = f"{sgh.sparkle_tmp_path}{sbatch_script_name}"
-    job_name = "--job-name=" + sbatch_script_name
-    std_out_path = sbatch_script_path + ".txt"
-    std_err_path = sbatch_script_path + ".err"
-    output = "--output=" + std_out_path
-    error = "--error=" + std_err_path
-    array = "--array=0-" + str(total_job_num - 1) + "%" + str(num_job_in_parallel)
-
-    sbatch_options_list = [job_name, output, error, array]
-    sbatch_options_list.extend(ssh.get_slurm_sbatch_default_options_list())
-    sbatch_options_list.extend(ssh.get_slurm_sbatch_user_options_list())
-    job_params_list = []
-
-    for job in total_job_list:
-        instance_path = job[0]
-        solver_path = job[1]
-        perf_m = sgh.settings.get_general_sparkle_objectives()[0].PerformanceMeasure
-        job_params_list.append(f"--instance {instance_path} --solver {solver_path}"
-                               f" --performance-measure {perf_m.name}")
-
-    srun_options_str = "-N1 -n1"
-    srun_options_str = srun_options_str + " " + ssh.get_slurm_srun_user_options_str()
-    target_call_str = "Commands/sparkle_help/run_solvers_core.py"
-
-    ssh.generate_sbatch_script_generic(sbatch_script_path, sbatch_options_list,
-                                       job_params_list, srun_options_str,
-                                       target_call_str)
-
-    return sbatch_script_path, std_out_path, std_err_path
 
 
 def running_solvers_parallel(
@@ -94,37 +53,32 @@ def running_solvers_parallel(
 
     # If there are no jobs, stop
     if num_jobs == 0:
-        return "" if run_on == Runner.SLURM else None
+        return None
     # If there are jobs update performance data ID
     else:
         srs.update_performance_data_id()
-
-    sbatch_script_path, std_out_path, std_err_path = (
-        generate_running_solvers_sbatch_shell_script(
-            num_jobs, num_job_in_parallel, jobs))
-
-    # Log output paths
-    sl.add_output(sbatch_script_path, "Slurm batch script to run solvers in parallel")
-    sl.add_output(std_out_path,
-                  "Standard output of Slurm batch script to run solvers in parallel")
-    sl.add_output(std_err_path,
-                  "Error output of Slurm batch script to run solvers in parallel")
 
     if run_on == Runner.LOCAL:
         print("Running the solvers locally")
     elif run_on == Runner.SLURM:
         print("Running the solvers through Slurm")
+    
+    srun_options = ["-N1", "-n1"] + ssh.get_slurm_srun_user_options_list()
+    sbatch_options = ssh.get_slurm_sbatch_default_options_list() +\
+        ssh.get_slurm_sbatch_user_options_list()
+    cmd_base = "Commands/sparkle_help/run_solvers_core.py"
+    perf_m = sgh.settings.get_general_sparkle_objectives()[0].PerformanceMeasure
+    cmd_list = [f"{cmd_base} --instance {inst_p} --solver {solver_p} "
+                f"--performance-measure {perf_m.name}" for inst_p, solver_p in jobs]
 
-    batch = SlurmBatch(sbatch_script_path)
-
-    cmd_list = [f"{batch.cmd} {param}" for param in batch.cmd_params]
     run = rrr.add_to_queue(
         runner=run_on,
         cmd=cmd_list,
+        parallel_jobs=num_job_in_parallel,
         name=CommandName.RUN_SOLVERS,
         base_dir=sgh.sparkle_tmp_path,
-        sbatch_options=batch.sbatch_options,
-        srun_options=batch.srun_options)
+        sbatch_options=sbatch_options,
+        srun_options=srun_options)
 
     if run_on == Runner.SLURM:
         # Add the run to the list of active job.
