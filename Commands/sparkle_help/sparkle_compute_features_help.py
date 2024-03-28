5203f64a84087799f2731fcb3175bb2a62c9ff5b
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-03-28 14:27:01 +0100

    Test fix

--- Commands/sparkle_help/sparkle_compute_features_help.py
+++ Commands/sparkle_help/sparkle_compute_features_help.py
@@ -277,4 +277,4 @@ def get_feature_data_id() -> int:
     fd_id_path = Path(sgh.feature_data_id_path)
     if not fd_id_path.exists():
         return 0
-    return Path(fd_id_path).open("r").readline()
+    return int(Path(fd_id_path).open("r").readline())
