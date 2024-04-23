9bd7727061e10b6d76a3bb22ed890baa424a55f6
Noah Peil <noah.peil@rwth-aachen.de>
2024-04-23 14:05:40 +0200

    Update to the validator class

--- Commands/structures/validator.py
+++ Commands/structures/validator.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import os
 import sys
 from pathlib import Path, PurePath
 import json
@@ -13,17 +14,40 @@ from Commands.sparkle_help import sparkle_basic_help as sbh
 from Commands.sparkle_help import sparkle_slurm_help as ssh
 from Commands.sparkle_help.sparkle_command_help import CommandName
 from Commands.sparkle_help import sparkle_file_help as sfh
+from Commands.sparkle_help import sparkle_run_configured_solver_help as srcsh
 
 
 class Validator():
     def __init__(self: Validator) -> None:
         """Construct the validator"""
-        # TODO: what attributes should the validator have?
         pass
 
-    def validate(self: Validator, solvers: list[Path], config_str_list: list[str] | str, instance_sets: list[Path], run_on: Runner=Runner.SLURM):
+    def get_validation_results(self, solver, instance_set, config=None):
+        # get raw results and put them in csv format
+        # one csv for each solver/instance_set combination
+        # Output/validation/solver_instanceset/validation.csv
+        #   within that file, we store both the configured and non-configured
+        #   runs. For non-configured, the corresponding row has None in the 
+        #   config column.
+
+        # go over all instances in the instance_set. For each such instance,
+        # pair it with the solver and try to find the corresponding result
+        # with the help of glob
+
+        # sparkle_tmp_path = "Tmp/"
+
+        # how would we even differentiate between configured and non-configured result?
+        # the raw_result_path is created with solver+instance+PID
+
+        for instance in instance_set:
+            pass
+        
+        return self._validation_results
+
+    def validate(self: Validator, solvers: list[Path], config_str_list: list[str] | str,
+                 instance_sets: list[Path], run_on: Runner=Runner.SLURM):
         """
-        TODO
+        TODO description
 
         Parameters
         ----------
@@ -32,31 +56,50 @@ class Validator():
         instance_sets: set of instance sets on which we want to validate each solver
         run_on: whether to run on SLURM or local
         """
-        # if there is only one configuration, we extend it to a list of the same length as the solver list
+        # if there is only one configuration, we extend it to a list of the same
+        #   length as the solver list
         if isinstance(config_str_list, str):
             config_str_list = [config_str_list] * len(solvers)
         elif len(config_str_list) != len(solvers):
-            print("Error: Length of solver list and configuration string list does not match!")
+            print("Error: Number of solvers and configurations does not match!")
             sys.exit(-1)
 
+        # gather slurm information for the non-configured solvers which are run
+        #   directly via runrunner
         num_job_in_parallel = sgh.settings.get_slurm_number_of_runs_in_parallel()
         # TODO: determine number of CPU cores we should use
         srun_options = ["-N1", "-n1"] + ssh.get_slurm_options_list()
         sbatch_options = ssh.get_slurm_options_list()
-        cmd_base = "Commands/sparkle_help/run_solvers_core.py"
         perf_m = sgh.settings.get_general_sparkle_objectives()[0].PerformanceMeasure
 
-        for solver in solvers:
-            for instance_set in instance_sets:
-                cmd_list = [f"{cmd_base} --instance {instance} --solver {solver} "
-                            f"--performance-measure {perf_m.name}" for instance in instance_set]
-                
-                run = rrr.add_to_queue(
-                    runner=run_on,
-                    cmd=cmd_list,
-                    parallel_jobs=num_job_in_parallel,
-                    name=CommandName.RUN_SOLVERS,
-                    base_dir=sgh.sparkle_tmp_path,
-                    sbatch_options=sbatch_options,
-                    srun_options=srun_options)
-                
+        for solver, config_str in zip(solvers, config_str_list):
+            # run a configured solver
+            if config_str is not None:
+                for instance_set in instance_sets:
+                    instance_path_list = list(instance_set.iterdir())
+                    _, solver_name = os.path.split(solver)
+                    srcsh.run_configured_solver(instance_path_list=instance_path_list,
+                                                solver_name=solver_name,
+                                                config_str=config_str)
+
+            # run a non-configured solver
+            else:
+                cmd_base = "Commands/sparkle_help/run_solvers_core.py"
+                for instance_set in instance_sets:
+                    cmd_list = [f"{cmd_base} --instance {instance} --solver {solver} "
+                                f"--performance-measure {perf_m.name}" for instance in instance_set]
+                    
+                    run = rrr.add_to_queue(
+                        runner=run_on,
+                        cmd=cmd_list,
+                        parallel_jobs=num_job_in_parallel,
+                        name=CommandName.RUN_SOLVERS,
+                        base_dir=sgh.sparkle_tmp_path,
+                        sbatch_options=sbatch_options,
+                        srun_options=srun_options)
+                    
+
+        if run_on == Runner.LOCAL:
+            print("Waiting for the local calculations to finish.")
+            run.wait()
+    
