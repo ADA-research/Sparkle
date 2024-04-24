22561979d14c2fe54960f4374561af5c8059cf5b
Brian Schiller <brian.schiller@rwth-aachen.de>
2024-04-24 14:49:14 +0200

    leftover

--- CLI/run_ablation.py
+++ CLI/run_ablation.py
@@ -103,7 +103,7 @@ def parser_function() -> argparse.ArgumentParser:
 if __name__ == "__main__":
     # Initialise settings
     global settings
-    sgh.settings = sparkle_settings.Settings()
+    sgh.settings = settings_help.Settings()
 
     sl.log_command(sys.argv)
 
