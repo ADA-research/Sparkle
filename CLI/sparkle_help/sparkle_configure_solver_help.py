f214f2f39c8436bbecc700a51d3125a07bfc3db7
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-04-24 13:31:09 +0200

    Refactoring pcs methods into their own file

--- CLI/sparkle_help/sparkle_configure_solver_help.py
+++ CLI/sparkle_help/sparkle_configure_solver_help.py
@@ -264,95 +264,20 @@ def check_validation_prerequisites() -> None:
     check_configuration_permission_error()
 
 
-def write_optimised_configuration_str(solver_name: str, instance_set_name: str) -> None:
+def write_configuration_str(configuration_str: str) -> None:
     """Write the latest optimised configuration parameter string to file.
 
     Args:
         solver_name: Name of the solver
         instance_set_name: Name of the instance set
     """
-    optimised_configuration_str, _, _ = get_optimised_configuration(
-        solver_name, instance_set_name)
     latest_configuration_str_path = sgh.sparkle_tmp_path + "latest_configuration.txt"
 
     with Path(latest_configuration_str_path).open("w") as outfile:
-        outfile.write(optimised_configuration_str)
+        outfile.write(configuration_str)
 
     sl.add_output(latest_configuration_str_path, "Configured algorithm parameters of the"
                   " most recent configuration process")
-
-
-def write_optimised_configuration_pcs(solver_name: str, instance_set_name: str) -> None:
-    """Write optimised configuration to a new PCS file.
-
-    Args:
-        solver_name: Name of the solver
-        instance_set_name: Name of the instance set
-    """
-    # Read optimised configuration and convert to dict
-    optimised_configuration_str, _, _ = get_optimised_configuration(
-        solver_name, instance_set_name)
-    optimised_configuration_str += " -arena '12345'"
-    optimised_configuration_list = optimised_configuration_str.split()
-
-    # Create dictionary
-    config_dict = {}
-    for i in range(0, len(optimised_configuration_list), 2):
-        # Remove dashes and spaces from parameter names, and remove quotes and
-        # spaces from parameter values before adding them to the dict
-        config_dict[optimised_configuration_list[i].strip(" -")] = (
-            optimised_configuration_list[i + 1].strip(" '"))
-
-    # Read existing PCS file and create output content
-    solver_directory = Path("Solvers", solver_name)
-    pcs_file = solver_directory / pcs.get_pcs_file_from_solver_directory(
-        solver_directory)
-    pcs_file_out = []
-
-    with Path(pcs_file).open() as infile:
-        for line in infile:
-            # Copy empty lines
-            if not line.strip():
-                line_out = line
-            # Don't mess with conditional (containing '|') and forbidden (starting
-            # with '{') parameter clauses, copy them as is
-            elif "|" in line or line.startswith("{"):
-                line_out = line
-            # Also copy parameters that do not appear in the optimised list
-            # (if the first word in the line does not match one of the parameter names
-            # in the dict)
-            elif line.split()[0] not in config_dict:
-                line_out = line
-            # Modify default values with optimised values
-            else:
-                words = line.split("[")
-                if len(words) == 2:
-                    # Second element is default value + possible tail
-                    param_name = line.split()[0]
-                    param_val = config_dict[param_name]
-                    tail = words[1].split("]")[1]
-                    line_out = words[0] + "[" + param_val + "]" + tail
-                elif len(words) == 3:
-                    # Third element is default value + possible tail
-                    param_name = line.split()[0]
-                    param_val = config_dict[param_name]
-                    tail = words[2].split("]")[1]
-                    line_out = words[0] + words[1] + "[" + param_val + "]" + tail
-                else:
-                    # This does not seem to be a line with a parameter definition, copy
-                    # as is
-                    line_out = line
-            pcs_file_out.append(line_out)
-
-    latest_configuration_pcs_path = sgh.sparkle_tmp_path + "latest_configuration.pcs"
-
-    with Path(latest_configuration_pcs_path).open("w") as outfile:
-        for element in pcs_file_out:
-            outfile.write(str(element))
-    # Log output
-    sl.add_output(latest_configuration_pcs_path, "PCS file with configured algorithm "
-                  "parameters of the most recent configuration process as default "
-                  "values")
 
 
 def check_optimised_configuration_params(params: str) -> None:
