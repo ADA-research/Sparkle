740062f131426c7cbd101f250c4950b08af9b3c5
Noah Peil <noah.peil@rwth-aachen.de>
2024-04-24 14:20:42 +0200

    Refactoring

--- CLI/sparkle_help/sparkle_run_solvers_parallel_help.py
+++ CLI/sparkle_help/sparkle_run_solvers_parallel_help.py
@@ -10,7 +10,7 @@ from runrunner.base import Runner
 from CLI.sparkle_help import sparkle_global_help as sgh
 from sparkle.structures.performance_dataframe import PerformanceDataFrame
 from CLI.sparkle_help import sparkle_run_solvers_help as srs
-from sparkle.sparkle.platform import slurm_help as ssh
+from sparkle.platform import slurm_help as ssh
 from CLI.help.command_help import CommandName
 
 
