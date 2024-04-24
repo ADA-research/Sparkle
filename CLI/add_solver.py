644fc6e98e7a50a4abda2f0c31e66556319d66bc
J. Rook <jrook94@gmail.com>
2024-04-24 12:12:48 +0200

    Move solver add

--- CLI/add_solver.py
+++ CLI/add_solver.py
@@ -15,7 +15,7 @@ from CLI.sparkle_help import sparkle_global_help as sgh
 from sparkle.sparkle_performance_dataframe import PerformanceDataFrame
 from CLI.sparkle_help import sparkle_run_solvers_help as srs
 from CLI.sparkle_help import sparkle_run_solvers_parallel_help as srsp
-from CLI.sparkle_help import sparkle_add_solver_help as sash
+from sparkle.solver import add as sash
 from CLI.sparkle_help import sparkle_logging as sl
 from CLI.sparkle_help import sparkle_settings
 from CLI.sparkle_help.sparkle_command_help import CommandName
