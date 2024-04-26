cf58737fcb3695e23697a9a3114850ab4ab8cf4e
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-04-26 14:41:47 +0200

    Refactoring, bugfix

--- CLI/validate_configured_vs_default.py
+++ CLI/validate_configured_vs_default.py
@@ -10,7 +10,6 @@ from runrunner.base import Runner
 import global_variables as sgh
 from CLI.support import configure_solver_help as scsh
 from sparkle.solver import pcs
-from sparkle.instance import instances_help as sih
 import sparkle_logging as sl
 from sparkle.types.objective import PerformanceMeasure
 from sparkle.platform.settings_help import SettingState
@@ -99,18 +98,15 @@ if __name__ == "__main__":
     instance_set_test = args.instance_set_test
     run_on = args.run_on
 
-    if args.configurator is not None:
-        sgh.settings.set_general_sparkle_configurator(
-            value=getattr(Configurator, args.configurator),
-            origin=SettingState.CMD_LINE)
-    configurator = sgh.settings.get_general_sparkle_configurator()
-    configurator.set_scenario_dirs(Path(solver).name, instance_set_train.name)
-
+    
     check_for_initialise(
         sys.argv,
         ch.COMMAND_DEPENDENCIES[ch.CommandName.VALIDATE_CONFIGURED_VS_DEFAULT]
     )
-
+    if args.configurator is not None:
+        sgh.settings.set_general_sparkle_configurator(
+            value=getattr(Configurator, args.configurator),
+            origin=SettingState.CMD_LINE)
     if ac.set_by_user(args, "settings_file"):
         sgh.settings.read_settings_ini(
             args.settings_file, SettingState.CMD_LINE
@@ -127,9 +123,10 @@ if __name__ == "__main__":
             args.target_cutoff_time, SettingState.CMD_LINE
         )
 
-    instance_set_test_name = None
 
     # Make sure configuration results exist before trying to work with them
+    configurator = sgh.settings.get_general_sparkle_configurator()
+    configurator.set_scenario_dirs(Path(solver).name, instance_set_train.name)
     scsh.check_validation_prerequisites()
 
     # Record optimised configuration
@@ -139,40 +136,13 @@ if __name__ == "__main__":
     pcs.write_configuration_pcs(solver.name, optimised_configuration_str,
                                 Path(sgh.sparkle_tmp_path))
 
-    if instance_set_test is not None:
-        instance_set_test_name = instance_set_test.name
-
-        # Create instance file list for test set in configurator
-        instances_directory_test = sgh.instance_dir / instance_set_test_name
-        list_path = sih.get_list_all_path(instances_directory_test)
-        instance_list_test_path = configurator.instances_path\
-            / instance_set_test_name / (instance_set_test_name + "_test.txt")
-        instance_list_test_path.parent.mkdir(parents=True, exist_ok=True)
-        with instance_list_test_path.open("w+") as fout:
-            for instance in list_path:
-                fout.write(f"{instance.absolute()}\n")
-
-    # Create solver execution directories, and copy necessary files there
-    scsh.prepare_smac_execution_directories_validation(instance_set_test_name)
-    configurator = sgh.settings.get_general_sparkle_configurator()
-
     validator = Validator()
     all_validation_instances = [instance_set_train]
-    if instance_set_test_name is not None:
+    if instance_set_test is not None:
         all_validation_instances.append(instance_set_test)
     config_str = scsh.get_optimised_configuration_params(solver, instance_set_train)
     validator.validate(solvers=[solver] * 2, config_str_list=[None, config_str],
                        instance_sets=all_validation_instances)
-
-    # Write most recent run to file
-    last_test_file_path =\
-        configurator.scenarios_path / f"{solver.name}_{sgh.sparkle_last_test_file_name}"
-
-    with Path(last_test_file_path).open("w+") as fout:
-        fout.write(f"solver {solver}\n"
-                   f"train {instance_set_train}\n")
-        if instance_set_test is not None:
-            fout.write(f"test {instance_set_test}\n")
 
     # Update latest scenario
     sgh.latest_scenario().set_config_solver(Path(solver))
