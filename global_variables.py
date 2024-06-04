472d0f8db127535a570c948206fd93ab20226234
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-06-04 06:53:21 +0200

    Refactoring gv method

--- global_variables.py
+++ global_variables.py
@@ -180,11 +180,5 @@ def get_time_pid_random_string() -> str:
     Returns:
       A random string composed of time, PID and a random positive integer value.
     """
-    my_time_str = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime(time.time()))
-    my_pid = os.getpid()
-    my_pid_str = str(my_pid)
-    my_random = random.randint(1, sys.maxsize)
-    my_random_str = str(my_random)
-    my_time_pid_random_str = my_time_str + "_" + my_pid_str + "_" + my_random_str
-
-    return my_time_pid_random_str
+    time_stamp = time.strftime("%Y-%m-%d-%H:%M:%S", time.localtime(time.time()))
+    return f"{time_stamp}_{os.getpid()}_{random.randint(1, sys.maxsize)}"
