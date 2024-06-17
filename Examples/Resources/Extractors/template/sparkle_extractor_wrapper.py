90c7b4ec41b2d554413a211856f088bedb54a8d3
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-06-17 09:30:35 +0200

    standardising

--- Examples/Resources/Extractors/template/sparkle_extractor_wrapper.py
+++ Examples/Resources/Extractors/template/sparkle_extractor_wrapper.py
@@ -2,8 +2,6 @@
 # -*- coding: UTF-8 -*-
 """Template for users to create Feature Extractor wrappers."""
 
-import sys
-import os
 import time
 import random
 import argparse
@@ -20,8 +18,8 @@ extractor_dir = args.extractor_dir
 instance_path = args.instance_file
 output_file = args.output_file
 
+# Set this to your executable descriptions
 extractor_name = "Example"
-
 executable_name = "features"
 executable = Path(extractor_dir) / executable_name
 
@@ -35,24 +33,13 @@ command_line = [Path(extractor_dir) / executable_name, instance_path, tmp_output
 
 subprocess.run(command_line, stdout=raw_result_file_name.open("w+"))
 
-# Read all lines from the input file
-raw_lines = Path(raw_result_file_name).read_text().splitlines()
+# Read all lines from the raw output file
+raw_text = Path(raw_result_file_name).read_text()
 
 # Process raw result file and write to the final result file
 with open(output_file, 'w') as out_file:
-    for idx, current_line in enumerate(raw_lines):
-        stripped_line = current_line.strip()
-        if not stripped_line or stripped_line.startswith('c'):
-            continue
-
-        # Split the line by commas, write each part to the output file with additional information
-        features = stripped_line.split(',')
-        out_file.write(','.join(f',{feature}{extractor_name}' for feature in features) + '\n')
-
-        # Ensure not to go out of index range for the next line
-        if idx + 1 < len(raw_lines):
-            next_line = raw_lines[idx + 1].strip()
-            out_file.write(f'{instance_path},{next_line}\n')
+    # Do some post processing if needed here
+    out_file.write(raw_text)
 
 # Deletes temporary files
 raw_result_file_name.unlink(missing_ok=True)
