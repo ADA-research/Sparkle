1f613f8a82b508d38b61bc546d9b59a43043772b
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-07-18 13:53:08 +0200

    Creating manifest, updating setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ setup(name="SparkleAI",
       url="https://github.com/thijssnelleman/Sparkle",
       author="Thijs Snelleman",
       author_email="fkt_sparkle@aim.rwth-aachen.de",
+      keywords="ai sat planning",
       description="Sparkle is a Programming by Optimisation (PbO)-based problem-solving "
                   "platform designed to enable the widespread and effective use of PbO "
-                  "techniques for improving the state-of-the-art in solving a broad range"
-                  " of prominent AI problems, including SAT and AI Planning.",
+                  "techniques for improving the state-of-the-art in solving a broad "
+                  "range of prominent AI problems, including SAT and AI Planning.",
       long_description=open("README.MD", "r").read() if os.path.exists("README.MD") else "",
       long_description_content_type="text/markdown",
       packages=find_packages(exclude=["tests"]),
+      include_package_data=True,
+      #data_files=["Components"],
       entry_points={"console_scripts": ["sparkle=sparkle.cli:main"], },)
