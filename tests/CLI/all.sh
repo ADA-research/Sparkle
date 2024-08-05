316fddd35c7a5333bd510a2695c940f08b184c20
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-08-05 15:11:50 +0200

    test renaming

--- tests/CLI/all.sh
+++ tests/CLI/all.sh
@@ -81,7 +81,7 @@ tests/CLI/run_configured_solver.sh
 tests/CLI/run_parallel_portfolio.sh
 
 # Test generate report (selection)
-tests/CLI/generate_report__for_selection.sh
+tests/CLI/generate_report_for_selection.sh
 
 # Test generate report for configuration
 tests/CLI/generate_report_for_configuration.sh
