8b07b842a5cf2df90eaf5740bdf9b4ad85e08c03
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-09-19 10:11:37 +0200

    Auto generate example .sh with code

--- Documentation/md_to_sh.py
+++ Documentation/md_to_sh.py
@@ -1,15 +1,27 @@
 """Generate shell scripts from markdown files."""
 
 from pathlib import Path
-
+from sparkle import about
 
 if __name__ == "__main__":
-    files = [p for p in Path.iterdir() if p.is_file() and p.suffix == ".md"]
+    files = [p for p in Path("../Examples").iterdir() if p.is_file() and p.suffix == ".md"]
 
     for file in files:
         with file.open() as f:
-            lines = f.readlines()
-
+            input_lines = f.readlines()
+        output_lines = []
+        code_block = False
+        for line in input_lines:
+            if line.startswith("```"):
+                code_block = not code_block
+                continue
+            if not code_block:
+                if line.strip() != "" and line[0] != "#":
+                    line = "# " + line
+            output_lines.append(line)
+            
         with file.with_suffix(".sh").open("w") as f:
             f.write("#!/usr/bin/env bash\n")
-            f.writelines(lines)
+            f.write("# Auto-Generated .sh files from the original .md by "
+                    f"Sparkle {about.version}")
+            f.writelines(output_lines)
