2f2ce05a836d3f4f0f8d9a5517d19510afa15d27
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-09-27 12:11:29 +0200

    New method for testing CLI entry points

--- tests/CLI/test_initialise.py
+++ tests/CLI/test_initialise.py
@@ -1,15 +1,22 @@
 """Test the initiliase CLI entry point."""
 import pytest
 from pathlib import Path
-import subprocess
+import os
+
+from sparkle.CLI import initialise
 
 
 @pytest.mark.integration
 def test_initialise_command(tmp_path: Path) -> None:
     """Test initialise command."""
     # Smoke test
-    call = subprocess.run(["sparkle", "initialise"],
-                          cwd=tmp_path)
-    assert call.returncode == 0
-
+    cwd = Path.cwd()
+    os.chdir(tmp_path)  # Execute the command in the PyTest tmp directory
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        # Call the command
+        initialise.main([])
+        # Check the exit status
+        assert pytest_wrapped_e.type is SystemExit
+        assert pytest_wrapped_e.value.code == 0
+    os.chdir(cwd)
     # TODO: Check with/without specific error messages
