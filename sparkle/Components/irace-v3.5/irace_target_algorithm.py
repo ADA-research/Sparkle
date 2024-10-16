f348b237fc725469213ebc37d50872de6c0a2fa6
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-10-16 10:42:56 +0200

    Updating target algorithms

---
+++ sparkle/Components/irace-v3.5/irace_target_algorithm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+"""Handles IRACE calls passing to sparkle solver wrappers."""
+import sys
+# from pathlib import Path
+
+# from runrunner import Runner
+
+# from sparkle.solver import Solver
+# from sparkle.types import resolve_objective
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    print(args)
+    sys.exit()
