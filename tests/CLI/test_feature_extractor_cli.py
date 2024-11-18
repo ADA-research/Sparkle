548608f91670ded16cae4373b80436088c3815c2
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-11-18 15:23:39 +0100

    Adding no-copy to feature extractor

--- tests/CLI/test_feature_extractor_cli.py
+++ tests/CLI/test_feature_extractor_cli.py
@@ -13,16 +13,33 @@ def test_add_remove_solver_command(tmp_path: Path,
                       / "SAT-features-competition2012_"
                         "revised_without_SatELite_sparkle").absolute()
     monkeypatch.chdir(tmp_path)  # Execute in PyTest tmp dir
+    expected_target = Path("Extractors") / extractor_path.name
     # Smoke test
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         add_feature_extractor.main([str(extractor_path)])
     assert pytest_wrapped_e.type is SystemExit
     assert pytest_wrapped_e.value.code == 0
+    assert expected_target.exists()
 
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         remove_feature_extractor.main([extractor_path.name])
     assert pytest_wrapped_e.type is SystemExit
     assert pytest_wrapped_e.value.code == 0
+    assert not expected_target.exists()
+
+    # Test with symbolic link
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        add_feature_extractor.main([str(extractor_path.absolute()),
+                                    "--no-copy"])
+    assert pytest_wrapped_e.type is SystemExit
+    assert pytest_wrapped_e.value.code == 0
+    assert expected_target.is_symlink()
+
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        remove_feature_extractor.main([extractor_path.name])
+    assert pytest_wrapped_e.type is SystemExit
+    assert pytest_wrapped_e.value.code == 0
+    assert not expected_target.exists()
 
 # TODO: Test removing with extractor directory path
 # TODO: Test adding / removing with nicknames
