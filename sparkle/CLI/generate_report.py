f9932ef5cdea98266b5b7584e146ace50a111bc2
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2024-12-12 12:13:18 +0100

    Fixed command specification

--- sparkle/CLI/generate_report.py
+++ sparkle/CLI/generate_report.py
@@ -239,7 +239,8 @@ def main(argv: list[str]) -> None:
         configuration_jobs = performance_data.get_job_list()
         if len(configuration_jobs) > 0:
             print(f"ERROR: {(len(configuration_jobs))} jobs for the configuration were "
-                  "not executed! Please run 'sparkle run solvers' before continuing.")
+                  "not executed! Please run 'sparkle run solvers --performance-data' "
+                  "before continuing.")
             sys.exit(-1)
         # Create machine readable output
         output = gv.settings().DEFAULT_configuration_output_analysis
