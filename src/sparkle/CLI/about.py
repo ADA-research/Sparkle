3dfe99ba24d2ad251a66e79e54743d5b290e7295
Thijs Snelleman <snelleman@aim.rwth-aachen.de>
2025-10-07 14:02:17 +0200

    authors bugfix

--- src/sparkle/CLI/about.py
+++ src/sparkle/CLI/about.py
@@ -18,14 +18,14 @@ def parser_function() -> argparse.ArgumentParser:
 
 def main(argv: list[str]) -> None:
     """Main function of the command."""
+    authors = sparkle.__about__.authors.split(", ")
     print(
         "\n".join(
             [
                 f"Sparkle ({sparkle.__about__.description})",
                 f"Version: {sparkle.__about__.__version__}",
                 f"Licence: {sparkle.__about__.licence}",
-                f"Written by {', '.join(sparkle.__about__.authors[:-1])},\
-           and {sparkle.__about__.authors[-1]}",
+                f"Written by {', '.join(authors[:-1])} and {authors[-1]}",
                 f"Contact: {sparkle.__about__.contact}",
                 "For more details see README.md",
             ]
